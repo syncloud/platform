5132be0fa2071acdcf3bf775c42871e36673354a
Boris Rybalkin <ribalkin@gmail.com>
2015-03-29 14:56:51 +0100

    merging image-boot into the platform

--- setup.py
+++ setup.py
@@ -23,11 +23,23 @@ setup(
         'bin/syncloud-insider-post-install',
         'bin/syncloud-base-installer',
         'bin/install-java',
-        'bin/syncloud-image-base-post-install'
+        'bin/syncloud-image-base-post-install',
+        'bin/syncloud-link-data.sh',
+        'bin/syncloud-boot-installer',
+        'bin/syncloud-resize-sd',
+        'bin/syncloud-resize-sd-partition'
     ],
     packages=['syncloud', 'syncloud.insider'],
     namespace_packages=['syncloud'],
-    data_files=[('insider/config', ['config/insider.cfg'])],
+    data_files=[
+        ('insider/config', ['config/insider.cfg']),
+        ('syncloud-image-boot/config', ['config/udisks/udisks-glue.conf']),
+        ('/etc/sudoers.d', ['config/sudoers.d/www-data']),
+        ('/etc/polkit-1/localauthority/50-local.d', ['config/polkit/55-storage.pkla']),
+        ('/etc/udev/rules.d', ['config/udev/99-syncloud.udisks.rules']),
+        ('/lib/systemd/system', ['config/systemd/ntpdate.service', 'config/systemd/udisks-glue.service']),
+        ('/etc/init.d', ['bin/syncloud-resize-sd'])
+    ],
     install_requires=requirements,
     description='Syncloud platform',
     long_description='Syncloud platform',
