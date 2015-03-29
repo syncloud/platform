2a8bf61785b4cd3f30b2e8b7a57716c41aae2b35
Boris Rybalkin <ribalkin@gmail.com>
2015-03-29 23:10:16 +0100

    pip non develop mode loses coverage, switching back to -e

--- test/server/test_cli.py
+++ test/server/test_cli.py
@@ -1,11 +1,7 @@
 from subprocess import check_output, CalledProcessError
-from os.path import join
-from syncloud.sam.manager import default_config_path, config_file
 
 
 def test_run():
-    print open(join(default_config_path, config_file), 'r').read()
-
     try:
         print check_output(['syncloud-cli', '-h'])
     except CalledProcessError, e:
