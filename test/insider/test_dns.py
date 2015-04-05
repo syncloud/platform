6247046e4f961024a892cf06b63bf2e0223d3347
Boris Rybalkin <ribalkin@gmail.com>
2015-04-05 02:25:59 +0100

    porting insider tests to pytest

--- test/insider/test_dns.py
+++ test/insider/test_dns.py
@@ -1,5 +1,5 @@
 import logging
-import unittest
+import pytest
 
 import responses
 from mock import MagicMock
@@ -30,38 +30,36 @@ def get_port_mapper(port_config, upnpc):
     return PortMapper(port_config, upnpc)
 
 
-class TestDns(unittest.TestCase):
+def assertSingleRequest(expected_request):
+    expected_request = reformat(expected_request)
+    assert 1 == len(responses.calls)
+    the_call = responses.calls[0]
+    assert expected_request == the_call.request.body
 
-    def assertSingleRequest(self, expected_request):
-        expected_request = reformat(expected_request)
-        self.assertEquals(1, len(responses.calls))
-        the_call = responses.calls[0]
-        self.assertEquals(expected_request, the_call.request.body)
+@responses.activate
+def test_sync_success():
+    service_config = get_service_config([
+        Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
+        Service("SSH", "https", "_http._tcp", 81, url=None)
+    ])
+    port_config = get_port_config([Port(80, 80), Port(81, 81)])
+    upnpc = get_upnpc(external_ip='192.167.44.52')
+    upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
 
-    @responses.activate
-    def test_sync_success(self):
-        service_config = get_service_config([
-            Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
-            Service("SSH", "https", "_http._tcp", 81, url=None)
-        ])
-        port_config = get_port_config([Port(80, 80), Port(81, 81)])
-        upnpc = get_upnpc(external_ip='192.167.44.52')
-        upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
+    port_mapper = get_port_mapper(port_config, upnpc)
+    domain_config = get_domain_config(Domain('boris', 'some_update_token'))
 
-        port_mapper = get_port_mapper(port_config, upnpc)
-        domain_config = get_domain_config(Domain('boris', 'some_update_token'))
+    responses.add(responses.POST,
+                  "http://api.domain.com/domain/update",
+                  status=200,
+                  body="{'message': 'Domain was updated'}",
+                  content_type="application/json")
 
-        responses.add(responses.POST,
-                      "http://api.domain.com/domain/update",
-                      status=200,
-                      body="{'message': 'Domain was updated'}",
-                      content_type="application/json")
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
+    dns.sync()
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-        dns.sync()
-
-        expected_request = '''
+    expected_request = '''
 {
     "services": [
         {"name": "ownCloud", "protocol": "http", "type": "_http._tcp", "port": 80, "local_port": 80, "url": "owncloud"},
@@ -72,32 +70,32 @@ class TestDns(unittest.TestCase):
     "token": "some_update_token"
 }
 '''
-        self.assertSingleRequest(expected_request)
+    assertSingleRequest(expected_request)
 
-    @responses.activate
-    def test_sync_server_side_client_ip(self):
-        service_config = get_service_config([
-            Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
-            Service("SSH", "https", "_http._tcp", 81, url=None)
-        ])
-        port_config = get_port_config([Port(80, 80), Port(81, 81)])
-        upnpc = get_upnpc(external_ip='10.1.1.1')
-        upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
+@responses.activate
+def test_sync_server_side_client_ip():
+    service_config = get_service_config([
+        Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
+        Service("SSH", "https", "_http._tcp", 81, url=None)
+    ])
+    port_config = get_port_config([Port(80, 80), Port(81, 81)])
+    upnpc = get_upnpc(external_ip='10.1.1.1')
+    upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
 
-        port_mapper = get_port_mapper(port_config, upnpc)
-        domain_config = get_domain_config(Domain('boris', 'some_update_token'))
+    port_mapper = get_port_mapper(port_config, upnpc)
+    domain_config = get_domain_config(Domain('boris', 'some_update_token'))
 
-        responses.add(responses.POST,
-                      "http://api.domain.com/domain/update",
-                      status=200,
-                      body="{'message': 'Domain was updated'}",
-                      content_type="application/json")
+    responses.add(responses.POST,
+                  "http://api.domain.com/domain/update",
+                  status=200,
+                  body="{'message': 'Domain was updated'}",
+                  content_type="application/json")
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-        dns.sync()
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
+    dns.sync()
 
-        expected_request = '''
+    expected_request = '''
 {
     "services": [
         {"name": "ownCloud", "protocol": "http", "type": "_http._tcp", "port": 80, "local_port": 80, "url": "owncloud"},
@@ -107,168 +105,168 @@ class TestDns(unittest.TestCase):
     "local_ip": "127.0.0.1"
 }
 '''
-        self.assertSingleRequest(expected_request)
+    assertSingleRequest(expected_request)
 
 
-    @responses.activate
-    def test_sync_server_error(self):
-        service_config = get_service_config([Service("ownCloud", "http", "_http._tcp", 80, url="owncloud")])
-        port_config = get_port_config([Port(80, 10000)])
-        upnpc = get_upnpc(external_ip='192.167.44.52')
-        port_mapper = get_port_mapper(port_config, upnpc)
-        domain_config = get_domain_config(Domain('boris', 'some_update_token'))
+@responses.activate
+def test_sync_server_error():
+    service_config = get_service_config([Service("ownCloud", "http", "_http._tcp", 80, url="owncloud")])
+    port_config = get_port_config([Port(80, 10000)])
+    upnpc = get_upnpc(external_ip='192.167.44.52')
+    port_mapper = get_port_mapper(port_config, upnpc)
+    domain_config = get_domain_config(Domain('boris', 'some_update_token'))
 
-        responses.add(responses.POST,
-                      "http://api.domain.com/domain/update",
-                      status=400,
-                      body='{"message": "Unknown update token"}',
-                      content_type="application/json")
+    responses.add(responses.POST,
+                  "http://api.domain.com/domain/update",
+                  status=400,
+                  body='{"message": "Unknown update token"}',
+                  content_type="application/json")
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
 
-        with self.assertRaises(PassthroughJsonError) as context:
-            dns.sync()
+    with pytest.raises(PassthroughJsonError) as context:
+        dns.sync()
 
-        self.assertEquals(context.exception.message, "Unknown update token")
+    assert context.value.message == "Unknown update token"
 
-    @responses.activate
-    def test_link_success(self):
-        config.footprints.append(('my-PC', footprint.footprint()))
-        config.titles['my-PC'] = 'My PC'
-        device_id = id.id()
+@responses.activate
+def test_link_success():
+    config.footprints.append(('my-PC', footprint.footprint()))
+    config.titles['my-PC'] = 'My PC'
+    device_id = id.id()
 
-        domain_config = get_domain_config(None)
+    domain_config = get_domain_config(None)
 
-        responses.add(responses.POST,
-                      "http://api.domain.com/domain/acquire",
-                      status=200,
-                      body='{"user_domain": "boris", "update_token": "some_update_token"}',
-                      content_type="application/json")
+    responses.add(responses.POST,
+                  "http://api.domain.com/domain/acquire",
+                  status=200,
+                  body='{"user_domain": "boris", "update_token": "some_update_token"}',
+                  content_type="application/json")
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config=None, port_mapper=None, local_ip='127.0.0.1')
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config=None, port_mapper=None, local_ip='127.0.0.1')
+    result = dns.acquire('boris@mail.com', 'pass1234', 'boris')
+
+    assert result is not None
+    assert result.user_domain == "boris"
+    assert result.update_token == "some_update_token"
+
+    expected_request_data = {
+        "password": "pass1234",
+        "email": "boris@mail.com",
+        "user_domain": "boris",
+        'device_mac_address': device_id.mac_address,
+        'device_name': device_id.name,
+        'device_title': device_id.title,
+    }
+    # Need to assert all passed POST parameters
+    # self.assertSingleRequest(convertible.to_json(expected_request_data))
+
+    domain = domain_config.load()
+    assert domain is not None
+    assert domain.user_domain == "boris"
+    assert domain.update_token == "some_update_token"
+
+@responses.activate
+def test_link_server_error():
+    config.footprints.append(('my-PC', footprint.footprint()))
+    config.titles['my-PC'] = 'My PC'
+
+    domain_config = get_domain_config(None)
+
+    responses.add(responses.POST,
+                  "http://api.domain.com/domain/acquire",
+                  status=403,
+                  body='{"message": "Authentication failed"}',
+                  content_type="application/json")
+
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config=None, port_mapper=None, local_ip='127.0.0.1')
+    with pytest.raises(PassthroughJsonError) as context:
         result = dns.acquire('boris@mail.com', 'pass1234', 'boris')
 
-        self.assertIsNotNone(result)
-        self.assertEquals(result.user_domain, "boris")
-        self.assertEquals(result.update_token, "some_update_token")
+    assert context.value.message == "Authentication failed"
 
-        expected_request_data = {
-            "password": "pass1234",
-            "email": "boris@mail.com",
-            "user_domain": "boris",
-            'device_mac_address': device_id.mac_address,
-            'device_name': device_id.name,
-            'device_title': device_id.title,
-        }
-        # Need to assert all passed POST parameters
-        # self.assertSingleRequest(convertible.to_json(expected_request_data))
+    domain = domain_config.load()
+    assert domain is None
 
-        domain = domain_config.load()
-        self.assertIsNotNone(domain)
-        self.assertEquals(domain.user_domain, "boris")
-        self.assertEquals(domain.update_token, "some_update_token")
+def test_add_service():
+    service_config = get_service_config([])
+    port_config = get_port_config([])
+    upnpc = get_upnpc(external_ip='192.167.44.52')
+    port_mapper = get_port_mapper(port_config, upnpc)
 
-    @responses.activate
-    def test_link_server_error(self):
-        config.footprints.append(('my-PC', footprint.footprint()))
-        config.titles['my-PC'] = 'My PC'
+    domain_config = get_domain_config(None)
 
-        domain_config = get_domain_config(None)
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
+    dns.add_service("ownCloud", "http", "_http._tcp", 80, url="owncloud")
 
-        responses.add(responses.POST,
-                      "http://api.domain.com/domain/acquire",
-                      status=403,
-                      body='{"message": "Authentication failed"}',
-                      content_type="application/json")
+    services = service_config.load()
+    assert 1 == len(services)
+    service = services[0]
+    assert "ownCloud" == service.name
+    assert "_http._tcp" == service.type
+    assert 80 == service.port
+    assert "owncloud" == service.url
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config=None, port_mapper=None, local_ip='127.0.0.1')
-        with self.assertRaises(PassthroughJsonError) as context:
-            result = dns.acquire('boris@mail.com', 'pass1234', 'boris')
+    mappings = port_config.load()
+    assert 1 == len(mappings)
+    mapping = mappings[0]
+    assert 80 == mapping.local_port
 
-        self.assertEquals(context.exception.message, "Authentication failed")
+def test_get_service():
+    service_config = get_service_config([])
+    port_config = get_port_config([])
+    upnpc = get_upnpc(external_ip='192.167.44.52')
+    port_mapper = get_port_mapper(port_config, upnpc)
 
-        domain = domain_config.load()
-        self.assertIsNone(domain)
+    domain_config = get_domain_config(None)
 
-    def test_add_service(self):
-        service_config = get_service_config([])
-        port_config = get_port_config([])
-        upnpc = get_upnpc(external_ip='192.167.44.52')
-        port_mapper = get_port_mapper(port_config, upnpc)
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
+    dns.add_service("ownCloud", "http", "_http._tcp", 80, url="owncloud")
 
-        domain_config = get_domain_config(None)
+    service = dns.get_service("ownCloud")
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-        dns.add_service("ownCloud", "http", "_http._tcp", 80, url="owncloud")
+    assert service is not None
+    assert "ownCloud" == service.name
+    assert "_http._tcp" == service.type
+    assert 80 == service.port
+    assert "owncloud" == service.url
 
-        services = service_config.load()
-        self.assertEquals(1, len(services))
-        service = services[0]
-        self.assertEquals("ownCloud", service.name)
-        self.assertEquals("_http._tcp", service.type)
-        self.assertEquals(80, service.port)
-        self.assertEquals("owncloud", service.url)
+def test_get_not_existing_service():
+    service_config = get_service_config([])
+    port_config = get_port_config([])
+    upnpc = get_upnpc(external_ip='192.167.44.52')
+    port_mapper = get_port_mapper(port_config, upnpc)
 
-        mappings = port_config.load()
-        self.assertEquals(1, len(mappings))
-        mapping = mappings[0]
-        self.assertEquals(80, mapping.local_port)
+    domain_config = get_domain_config(None)
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
 
-    def test_get_service(self):
-        service_config = get_service_config([])
-        port_config = get_port_config([])
-        upnpc = get_upnpc(external_ip='192.167.44.52')
-        port_mapper = get_port_mapper(port_config, upnpc)
+    service = dns.get_service("ownCloud")
 
-        domain_config = get_domain_config(None)
+    assert service is None
 
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-        dns.add_service("ownCloud", "http", "_http._tcp", 80, url="owncloud")
+def test_endpoints():
+    service_config = get_service_config([
+        Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
+        Service("SSH", "https", "_http._tcp", 81, url=None)
+    ])
+    port_config = get_port_config([Port(80, 8080), Port(81, 8181)])
+    upnpc = get_upnpc(external_ip='10.1.1.1')
+    # upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
 
-        service = dns.get_service("ownCloud")
+    port_mapper = get_port_mapper(port_config, upnpc)
+    domain_config = get_domain_config(Domain('boris', 'some_update_token'))
+    insider_config = get_insider_config('domain.com', 'http://api.domain.com')
+    dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
 
-        self.assertIsNotNone(service)
-        self.assertEquals("ownCloud", service.name)
-        self.assertEquals("_http._tcp", service.type)
-        self.assertEquals(80, service.port)
-        self.assertEquals("owncloud", service.url)
-
-    def test_get_not_existing_service(self):
-        service_config = get_service_config([])
-        port_config = get_port_config([])
-        upnpc = get_upnpc(external_ip='192.167.44.52')
-        port_mapper = get_port_mapper(port_config, upnpc)
-
-        domain_config = get_domain_config(None)
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-
-        service = dns.get_service("ownCloud")
-
-        self.assertIsNone(service)
-
-    def test_endpoints(self):
-        service_config = get_service_config([
-            Service("ownCloud", "http", "_http._tcp", 80, url="owncloud"),
-            Service("SSH", "https", "_http._tcp", 81, url=None)
-        ])
-        port_config = get_port_config([Port(80, 8080), Port(81, 8181)])
-        upnpc = get_upnpc(external_ip='10.1.1.1')
-        # upnpc.mapped_external_ports = MagicMock(side_effect=[[], [80]])
-
-        port_mapper = get_port_mapper(port_config, upnpc)
-        domain_config = get_domain_config(Domain('boris', 'some_update_token'))
-        insider_config = get_insider_config('domain.com', 'http://api.domain.com')
-        dns = Dns(insider_config, domain_config, service_config, port_mapper, '127.0.0.1')
-
-        endpoints = dns.endpoints()
-        assert len(endpoints) == 2
-        assert endpoints[0].service.name == 'ownCloud'
-        assert endpoints[0].external_port == 8080
-        assert endpoints[1].service.name == 'SSH'
-        assert endpoints[1].external_port == 8181
+    endpoints = dns.endpoints()
+    assert len(endpoints) == 2
+    assert endpoints[0].service.name == 'ownCloud'
+    assert endpoints[0].external_port == 8080
+    assert endpoints[1].service.name == 'SSH'
+    assert endpoints[1].external_port == 8181
