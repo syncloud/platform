61aa18d7f934b144e0e2f78574bbe875e01bc88c
Boris Rybalkin <ribalkin@gmail.com>
2015-04-05 14:53:42 +0100

    fix typos, more ssh keys checking

--- setup.py
+++ setup.py
@@ -47,7 +47,9 @@ setup(
         'bin/syncloud-server-post-upgrade',
         'bin/sam',
         'bin/syncloud-id',
-        'bin/syncloud-ping'
+        'bin/syncloud-ping',
+        'syncloud_ssh_keys_check',
+        'syncloud_ssh_keys_generate'
     ],
     packages=['syncloud', 'syncloud.insider', 'syncloud.remote', 'syncloud.apache', 'syncloud.server',
               'syncloud.sam', 'syncloud.app', 'syncloud.tools', 'syncloud.tools.cpu'],
