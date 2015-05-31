a2b0e6469ffad4e3d97f21b13c1ae573945131cc
Boris Rybalkin <ribalkin@gmail.com>
2015-05-31 01:17:46 +0100

    fixing test

--- integration/test.sh
+++ integration/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd ${DIR}
+APP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+cd ${APP_DIR}
 
 echo "=== install syncloud dependencies ==="
 wget --no-check-certificate --progress=dot:mega -O get-pip.py https://bootstrap.pypa.io/get-pip.py 2>&1
 python get-pip.py
 
 pip2 install -U pytest
-pip2 install -r /requirements.txt
+pip2 install -r dev_requirements.txt
 python setup.py
 pip2 freeze | grep syncloud
 
