9e4d0e44a62db62088208b0ca71ba30bd9bf8080
Boris Rybalkin <ribalkin@gmail.com>
2015-05-31 01:11:16 +0100

    adding pip

--- integration/test.sh
+++ integration/test.sh
@@ -3,11 +3,16 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-echo "rootfs version (docker):$(</version)"
-pip2 freeze | grep syncloud
+echo "=== install syncloud dependencies ==="
+wget --no-check-certificate --progress=dot:mega -O get-pip.py https://bootstrap.pypa.io/get-pip.py 2>&1
+python get-pip.py
+
 pip2 install -U pytest
 pip2 install -r /requirements.txt
 python setup.py
+pip2 freeze | grep syncloud
+
 syncloud-platform-post-install
+
 export TEAMCITY_VERSION=9
 py.test -s verify.py
