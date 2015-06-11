754e1e20f5f7ab3d787e9217483452ea00d061b3
vsapronov <vladimir.sapronov@gmail.com>
2015-06-11 09:18:19 -0400

    Some mombo-jombo with paths

--- syncloud/tools/env.py
+++ syncloud/tools/env.py
@@ -2,19 +2,15 @@ from sys import exec_prefix
 from os.path import join
 
 
-def is_virtual_env():
-    return not exec_prefix == '/usr'
-
-
 def usr_local_dir():
-    if not is_virtual_env():
+    if exec_prefix == '/usr':
         return join(exec_prefix, 'local')
     else:
         return exec_prefix
 
 
 def root_dir_prefix():
-    if not is_virtual_env():
+    if exec_prefix == '/usr':
         return ''
     else:
         return exec_prefix
