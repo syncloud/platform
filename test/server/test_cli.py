9ac548db4602f6e661dc4e45c2e664915fe24522
Boris Rybalkin <ribalkin@gmail.com>
2015-06-16 01:35:02 +0100

    no more server cli

--- test/server/test_cli.py
+++
@@ -1,9 +0,0 @@
-from subprocess import check_output, CalledProcessError
-
-
-def test_run():
-    try:
-        print check_output(['syncloud-cli', '-h'])
-    except CalledProcessError, e:
-        print(e.output)
-        raise e
