6873fc29e680f10c0ce081a7f60f2db9342f0825
vsapronov <vladimir.sapronov@gmail.com>
2015-06-19 09:33:17 -0400

    Hoot scripts for copy deployment

--- bin/run_hook.py
+++ bin/run_hook.py
@@ -1,8 +1,13 @@
-from syncloud.systemd.systemctl import add_service, remove_service
+import logging
+
+from syncloud.app import logger
 from syncloud.tools import app
 from syncloud.tools.nginx import Nginx
+from syncloud.systemd.systemctl import add_service, remove_service
+
 
 import sys
 
 script_filename = sys.argv[1]
+logger.init(logging.DEBUG, console=True, line_format='%(message)s')
 execfile(script_filename)
