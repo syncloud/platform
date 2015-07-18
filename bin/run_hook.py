d7522749810b9598451fd2f8b8a2d5b2a9fe80e1
Boris Rybalkin <ribalkin@gmail.com>
2015-07-18 11:41:35 +0100

    set __file__ to script file name so they can use this feature

--- bin/run_hook.py
+++ bin/run_hook.py
@@ -1,13 +1,9 @@
 import logging
-
 from syncloud_app import logger
-from syncloud_platform.tools import app
-from syncloud_platform.tools.nginx import Nginx
-from syncloud_platform.systemd.systemctl import add_service, remove_service
-
-
 import sys
 
 script_filename = sys.argv[1]
 logger.init(logging.DEBUG, console=True, line_format='%(message)s')
-execfile(script_filename)
+g = globals().copy()
+g['__file__'] = script_filename
+execfile(script_filename, g)
