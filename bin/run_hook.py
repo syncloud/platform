bf66bf7faedbeb132cb102c123c383b793a90ac0
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-27 10:03:18 -0400

    Added libs to sys.path for all tools

--- bin/run_hook.py
+++ bin/run_hook.py
@@ -1,6 +1,18 @@
 import logging
 from syncloud_app import logger
+
+# block to import lib folder
 import sys
+from os import listdir
+from os.path import join, dirname, isdir, abspath
+
+app_path = abspath(join(dirname(__file__), '..'))
+
+lib_path = join(app_path, 'lib')
+libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
+map(lambda x: sys.path.insert(0, x), libs)
+# end of block to import lib folder
+
 
 script_filename = sys.argv[1]
 logger.init(logging.DEBUG, console=True, line_format='%(message)s')
--- bin/uwsgi_internal.py
+++ bin/uwsgi_internal.py
@@ -1,2 +1,15 @@
+# block to import lib folder
+import sys
+from os import listdir
+from os.path import join, dirname, isdir, abspath
+
+app_path = abspath(join(dirname(__file__), '..'))
+
+lib_path = join(app_path, 'lib')
+libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
+map(lambda x: sys.path.insert(0, x), libs)
+# end of block to import lib folder
+
+
 import syncloud_platform.server.rest.internal
 app = syncloud_platform.server.rest.internal.app
