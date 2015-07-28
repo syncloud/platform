498de9403d94ced4cb5ae019fadaf534b56833ee
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-27 22:18:54 -0400

    Removed cmd tools that are not in use anymore

--- src/test/tools/test_cli.py
+++
@@ -1,8 +0,0 @@
-from subprocess import check_output
-from os.path import join, dirname
-
-
-# def test_local_ip():
-
-    # cmd = join(dirname(__file__), '..', 'bin', 'syncloud-id')
-    # check_output([cmd, 'local_ip'])
