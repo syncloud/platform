751cb029cb15c0cf4e1e80607cb7f2ddb2d95941
Boris Rybalkin <ribalkin@gmail.com>
2016-03-28 01:51:35 +0100

    fixing mapper test

--- src/test/insider/test_port_mapper.py
+++ src/test/insider/test_port_mapper.py
@@ -1,14 +1,18 @@
-
-
 import logging
+from miniupnpc import UPnP
 
 import pytest
 
 from syncloud_app import logger
-logger.init(level=logging.DEBUG, console=True)
 
+from syncloud_platform.insider.natpmpc import NatPmpPortMapper
+from syncloud_platform.insider.upnpc import UpnpClient
+from syncloud_platform.insider.upnpc import UpnpPortMapper
 from syncloud_platform.insider.port_drill import provide_mapper
 from test.insider.http import SomeHttpServer, wait_http, wait_http_cant_connect
+
+logger.init(level=logging.DEBUG, console=True)
+
 
 @pytest.fixture(scope="module")
 def http_server(request):
@@ -24,18 +28,21 @@ def http_server(request):
 ids = []
 mappers = []
 
-mapper = provide_mapper()
+mapper = provide_mapper(NatPmpPortMapper(), UpnpPortMapper(UpnpClient(UPnP())))
 if mapper is not None:
-    ids.append(mapper.__class__.__name__)
+    ids.append(mapper.name())
     mappers.append(mapper)
+
 
 def pytest_generate_tests(metafunc):
     if 'mapper' in metafunc.fixturenames:
         metafunc.parametrize('mapper', mappers, ids=ids)
 
+
 def test_external_ip(mapper):
     external_ip = mapper.external_ip()
     assert external_ip is not None
+
 
 def test_add_mapping_simple(http_server, mapper):
     external_port = mapper.add_mapping(http_server.port, http_server.port)
@@ -44,10 +51,12 @@ def test_add_mapping_simple(http_server, mapper):
     response = wait_http(external_ip, external_port, 200, timeout=1)
     assert response is not None
 
+
 def test_add_mapping_twice(http_server, mapper):
     external_port_first = mapper.add_mapping(http_server.port, http_server.port)
     external_port_second = mapper.add_mapping(http_server.port, http_server.port)
     assert external_port_first == external_port_second
+
 
 def test_remove_mapping(http_server, mapper):
     external_ip = mapper.external_ip()
