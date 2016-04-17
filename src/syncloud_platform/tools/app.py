706e09b29c1ca443cdbf3434bef034b457a9f2f7
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-17 15:54:23 -0400

    Removed unused methods

--- src/syncloud_platform/tools/app.py
+++ src/syncloud_platform/tools/app.py
@@ -30,32 +30,3 @@ def get_app_data_dir(app_name, remove_existing=False):
         print("app data dir exists: {0}".format(app_data_dir))
 
     return app_data_dir
-
-
-def get_app_data_root(app_name, user=None):
-    config = PlatformConfig()
-    if not os.path.isdir(config.data_root()):
-        print("creating app data root: {0}".format(config.data_root()))
-        os.mkdir(config.data_root())
-
-    return create_data_dir(config.data_root(), app_name, user)
-
-
-def create_data_dir(app_data_dir, dir_name, user=None, remove_existing=False):
-    data_dir = join(app_data_dir, dir_name)
-    print("checking app config folder: {0}".format(data_dir))
-
-    if os.path.isdir(data_dir) and remove_existing:
-        print("removing existing app data dir: {0}".format(data_dir))
-        shutil.rmtree(data_dir, ignore_errors=True)
-
-    if not os.path.isdir(data_dir):
-        print("creating app data dir: {0}".format(data_dir))
-        os.mkdir(data_dir)
-        if user:
-            print("setting permissions for {0} to {1}".format(data_dir, user))
-            os.chown(data_dir, getpwnam(user).pw_uid, getgrnam(user).gr_gid)
-    else:
-        print("app data dir exists: {0}".format(data_dir))
-
-    return data_dir
