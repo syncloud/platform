30675855eedb78ad4e06cd3328b5857250d2ab5f
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-17 18:12:37 -0400

    Forgot to add one more file

---
+++ src/syncloud_platform/gaplib/fs.py
@@ -0,0 +1,29 @@
+from os import makedirs, chown, utime
+from os.path import isdir
+from grp import getgrnam
+from pwd import getpwnam
+from shutil import rmtree
+
+def makepath(path):
+    if not isdir(path):
+        makedirs(path)
+
+
+def removepath(path):
+    if isdir(path):
+        rmtree(path, ignore_errors=True)
+
+
+def chownpath(path, user):
+    chown(path, getpwnam(user).pw_uid, getgrnam(user).gr_gid)
+
+
+def touch(file, user):
+    with open(file, 'a'):
+        utime(file, None)
+    chownpath(file, user)
+
+
+def chown(user, dir):
+    useradd(user)
+    return check_output('chown -RL {0}. {1}'.format(user, dir), shell=True)
