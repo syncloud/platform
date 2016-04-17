d0c2229f42c7ba1f110e9381cb8af44c573996f4
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-17 18:11:28 -0400

    Moving pure python calls to gaplib

---
+++ src/syncloud_platform/gaplib/linux.py
@@ -0,0 +1,27 @@
+from subprocess import check_output
+import pwd
+from os import environ
+import massedit
+
+
+def useradd(user):
+    try:
+        pwd.getpwnam(user)
+        return 'user {0} exists'.format(user)
+    except KeyError:
+        return check_output('/usr/sbin/useradd -r -s /bin/false {0}'.format(user), shell=True)
+
+
+def fix_locale():
+    if 'LANG' in environ:
+        lang = environ['LANG']
+        if lang not in check_output('locale -a 2>&1', shell=True):
+            print("generating locale: {0}".format(lang))
+            __fix_locale_gen(lang)
+            check_output('locale-gen')
+
+
+def __fix_locale_gen(lang, locale_gen='/etc/locale.gen'):
+    editor = massedit.MassEdit()
+    editor.append_code_expr("re.sub('# {0}', '{0}', line)".format(lang))
+    editor.edit_file(locale_gen)
