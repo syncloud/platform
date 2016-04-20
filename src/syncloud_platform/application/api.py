ae29280028c3618ae7d195031976a2c3178fc809
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-19 22:39:11 -0400

    Using application api for platform post-install

--- src/syncloud_platform/application/api.py
+++ src/syncloud_platform/application/api.py
@@ -1,5 +1,12 @@
-from syncloud_platform.application.apppaths import AppPaths
 from syncloud_platform.config.config import PlatformConfig
+from syncloud_platform.application.apppaths import AppPaths
+from syncloud_platform.application.appsetup import AppSetup
+from syncloud_platform.di.injector import get_injector
 
-def app_paths(app_name):
+def get_app_paths(app_name):
     return AppPaths(app_name, PlatformConfig())
+
+def get_app_setup(app_name):
+    app_paths = get_app_paths(app_name)
+    injector = get_injector()
+    app_setup = AppSetup(app_name, app_paths, injector.nginx)
