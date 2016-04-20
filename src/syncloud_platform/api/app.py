14085a1dde531ed606a39893042818122e607f24
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-20 18:08:28 -0400

    Using application api for post-install and pre-remove

--- src/syncloud_platform/api/app.py
+++
@@ -1,8 +0,0 @@
-from syncloud_platform.di.injector import get_injector
-
-def register_app(app, port):
-    get_injector().nginx.add_app(app, port)
-
-
-def unregister_app(app):
-    get_injector().nginx.remove_app(app)
