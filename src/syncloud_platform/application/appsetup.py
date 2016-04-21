684e377edae20d4723b88258349e72f75b350161
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-21 11:24:28 -0400

    Added domain and url info methods to application api

--- src/syncloud_platform/application/appsetup.py
+++ src/syncloud_platform/application/appsetup.py
@@ -1,10 +1,11 @@
 class AppSetup:
 
-    def __init__(self, app_name, app_paths, nginx, storage):
+    def __init__(self, app_name, app_paths, nginx, storage, device_info):
         self.app_name = app_name
         self.app_paths = app_paths
         self.nginx = nginx
         self.storage = storage
+        self.device_info = device_info
 
     def get_install_dir(self):
         return self.app_paths.get_install_dir()
@@ -20,4 +21,12 @@ class AppSetup:
 
     def init_storage(self, user):
         return self.storage.init_app_storage(self.app_name, user)
+
+    def device_domain_name(self):
+        return self.device_info.domain()
+
+    def app_url(self):
+        return self.device_info.url(self.app_name)
+
+
 
