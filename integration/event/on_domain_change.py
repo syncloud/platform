b0633113d4a54c37c6524d306f8084757edf91fe
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-21 11:55:41 -0400

    Fixing the build

--- integration/event/on_domain_change.py
+++ integration/event/on_domain_change.py
@@ -2,7 +2,7 @@
 from syncloud_platform.application import api
 
 app = api.get_app_setup('platform')
-domain = api.device_domain_name()
+domain = app.device_domain_name()
 
 with open('/tmp/on_domain_change.log', 'w+') as f:
     f.write(domain)
