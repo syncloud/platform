94d1ab1cbe82eb715c00bc7ffb85e6b0d30f2af2
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-21 11:41:34 -0400

    Using application api methods to get domain/url

--- integration/event/on_domain_change.py
+++ integration/event/on_domain_change.py
@@ -1,7 +1,8 @@
 #!/opt/app/platform/python/bin/python
-from syncloud_platform.api import info
+from syncloud_platform.application import api
 
-domain = info.domain()
+app = api.get_app_setup('platform')
+domain = api.device_domain_name()
 
 with open('/tmp/on_domain_change.log', 'w+') as f:
     f.write(domain)
