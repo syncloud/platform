d52c5952bcc93f1031701331bd3579f30c36c61b
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-06-29 10:10:42 -0400

    Trying to simplify unit tests running

--- unit-test.sh
+++ unit-test.sh
@@ -2,16 +2,9 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
-
-PYTHON=${DIR}/build/platform/python/bin
-export LD_LIBRARY_PATH=${DIR}/build/platform/python/lib
-
-wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
-${PYTHON}/python get-pip.py
-rm get-pip.py
+NAME="platform"
+BUILD_DIR=${DIR}/build/${NAME}
 
 cd ${DIR}/src
-${PYTHON}/python ${PYTHON}/pip2 install -U pytest
-${PYTHON}/python ${PYTHON}/pip2 install -r dev_requirements.txt
-
-${PYTHON}/py.test test
+${BUILD_DIR}/python/bin/pip install -r dev_requirements.txt
+${BUILD_DIR}/python/bin/py.test test
