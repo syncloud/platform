990f5f30f56d2cd361bc2fde33c1b1653fe33fb2
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-06-29 09:23:17 -0400

    Using syncloud_platform embeeded into python

--- bin/uwsgi_internal.py
+++ bin/uwsgi_internal.py
@@ -1,15 +1,2 @@
-# block to import lib folder
-import sys
-from os import listdir
-from os.path import join, dirname, isdir, abspath
-
-app_path = abspath(join(dirname(__file__), '..'))
-
-lib_path = join(app_path, 'lib')
-libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
-map(lambda x: sys.path.append(x), libs)
-# end of block to import lib folder
-
-
 import syncloud_platform.rest.internal
 app = syncloud_platform.rest.internal.app
