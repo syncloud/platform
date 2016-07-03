65dc30da6baee7fc3ecb31896dce7026fdf9901f
Boris Rybalkin <ribalkin@gmail.com>
2016-07-03 19:27:38 +0100

    upnp debug

--- src/syncloud_platform/insider/upnpc.py
+++ src/syncloud_platform/insider/upnpc.py
@@ -41,6 +41,7 @@ class UpnpClient:
             return
         self.logger.info('initializing upnp')
         self.upnp.discover()
+        self.logger.info(self.upnp.devlist)
         self.upnp.selectigd()
         self.initialized = True
 
