e7220b6053879965cc02f71949f93b2879de31a1
Boris Rybalkin <ribalkin@gmail.com>
2016-07-10 00:10:04 +0100

    fix docker cleanup

--- integration/docker.sh
+++ integration/docker.sh
@@ -52,7 +52,8 @@ function cleanup {
     docker images -q
 
     echo "removing images"
-    docker rm $(docker kill $(docker ps -qa))
+    docker kill $(docker ps -qa)
+    docker rm $(docker ps -qa)
     docker rmi $(docker images -q)
 
     echo "docker images"
