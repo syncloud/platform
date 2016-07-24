b456720d7c7c7a8ff70f77bdd1a8dc4eca967916
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-07-23 23:19:59 -0400

    Refactoring backend of app page

--- www/js/backend.static/app.js
+++ www/js/backend.static/app.js
@@ -1,27 +1,28 @@
-var app_data = {
-  "info": {
-    "app": {
-      "id": "owncloud",
-      "name": "ownCloud",
-      "required": false,
-      "icon": "penguin.png",
-      "ui": true,
-      "url": "/"
+var backend = {
+    app_data: {
+      "info": {
+        "app": {
+          "id": "owncloud",
+          "name": "ownCloud",
+          "required": false,
+          "icon": "penguin.png",
+          "ui": true,
+          "url": "/"
+        },
+        "current_version": "212",
+        "installed_version": "210"
+      }
     },
-    "current_version": "212",
-    "installed_version": "210"
-  }
-};
-
-function backend_app_action(app_id, action, on_complete) {
-    setTimeout(function() {
-        backend_update_app(app_id, on_complete);
-    }, 2000);
-}
-
-function backend_update_app(app_id, on_complete) {
-    setTimeout(function() {
-        ui_display_app(app_data);
-        on_complete();
-    }, 2000);
+    load_app: function(parameters) {
+        var that = this;
+        setTimeout(function() {
+            success_callbacks(parameters, that.app_data);
+        }, 2000);
+s   },
+    app_action: function(parameters) {
+        var that = this;
+        setTimeout(function() {
+            success_callbacks(parameters);
+        }, 2000);
+    }
 }
--- www/js/backend/app.js
+++ www/js/backend/app.js
@@ -1,16 +1,51 @@
-function backend_app_action(app_id, action, on_complete) {
-    $.get("/rest/"+action, {app_id: app_id})
-        .always(function() {
-            run_after_sam_is_complete(function() {
-                backend_update_app(app_id, on_complete);
+var backend = {
+    load_app: function(parameters) {
+        var app_id = parameters.app_id;
+        $.get( '/rest/app', {app_id: app_id})
+            .done(function (data) {
+                if (parameters.hasOwnProperty("done")) {
+                    parameters.done(data);
+                }
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                var error = null;
+                if (xhr.hasOwnProperty('responseJSON')) {
+                    var error = xhr.responseJSON;
+                }
+                if (parameters.hasOwnProperty("fail")) {
+                    parameters.fail(xhr.status, error);
+                }
+            })
+            .always(function() {
+                if (parameters.hasOwnProperty("always")) {
+                    parameters.always();
+                }
             });
-        });
-}
-
-function backend_update_app(app_id, on_complete) {
-    $.get( '/rest/app', {app_id: app_id})
-        .done( function(data) {
-            ui_display_app(data);
-        })
-        .always(on_complete);
+    },
+    app_action: function(parameters) {
+        var action = parameters.action;
+        var app_id = parameters.app_id;
+        $.get("/rest/"+action, {app_id: app_id})
+            .done(function(data) {
+                run_after_sam_is_complete(function() {
+                    if (parameters.hasOwnProperty("done")) {
+                        parameters.done(data);
+                    }
+                });
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                var error = null;
+                if (xhr.hasOwnProperty('responseJSON')) {
+                    var error = xhr.responseJSON;
+                }
+                if (parameters.hasOwnProperty("fail")) {
+                    parameters.fail(xhr.status, error);
+                }
+            })
+            .always(function() {
+                if (parameters.hasOwnProperty("always")) {
+                    parameters.always();
+                }
+            });
+    }
 }
