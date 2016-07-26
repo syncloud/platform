fb8d1c694e980d396fee42a7d765d44a879609a5
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-07-25 22:41:51 -0400

    All menu code goes to ui/menu.js, ui/font.js is for font loading

--- www/public/js/ui/menu.js
+++ www/public/js/ui/menu.js
@@ -1,46 +1,44 @@
-$(document).ready(function() { 
-	//fonts
-	WebFontConfig = {
-	google: { families: [ 'Roboto:400,300:latin' ] }
-	};
-	(function() {
-	var wf = document.createElement('script');
-	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-	'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-	wf.type = 'text/javascript';
-	wf.async = 'true';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(wf, s);
-	})(); 
-	
-	// navi
-	$(".menubutton").click(function(e) {
-	$(".navi").toggleClass("naviopen");
-	$(".menubutton").toggleClass("menuopen");
-	e.preventDefault();
-	});
-	$(".navi a, #block1, #block2, #block3, #block4, #block5, #block6, footer").click(function(){
-	$(".navi").removeClass("naviopen");
-	$(".menubutton").removeClass("menuopen");
-	});
-});
-/* When the user clicks on the button, 
-	toggle between hiding and showing the dropdown content */
-	function dropdown() {
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function dropdown() {
 	document.getElementById("myDropdown").classList.toggle("show");
-	}
+}
 
-	// Close the dropdown menu if the user clicks outside of it
-	window.onclick = function(event) {
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
 	if (!event.target.matches('.dropdown')) {
 
-	var dropdowns = document.getElementsByClassName("dropdown-content");
-	var i;
-	for (i = 0; i < dropdowns.length; i++) {
-	var openDropdown = dropdowns[i];
-	if (openDropdown.classList.contains('show')) {
-	openDropdown.classList.remove('show');
-	}
-	}
-	}
-	}
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+$( document ).ready(function() {
+    $("#btn_logout_large, #btn_logout_small").click(function(event) {
+        backend_menu.logout({
+            done: function(data) {
+                window.location.href = "login.html";
+            },
+            fail: on_error
+        });
+    });
+
+    $("#btn_restart_large, #btn_restart_small").click(function(event) {
+        backend_menu.restart({
+            done: function(data) {},
+            fail: on_error
+        });
+    });
+
+    $("#btn_shutdown_large, #btn_shutdown_small").click(function(event) {
+        backend_menu.shutdown({
+            done: function(data) {},
+            fail: on_error
+        });
+    });
+});
