c77cf767f12163ae32d07969e33c61a5b7a89730
Boris Rybalkin <ribalkin@gmail.com>
2016-08-18 20:47:48 +0100

    fixing loop cleanup

--- integration/util/loop.py
+++ integration/util/loop.py
@@ -9,8 +9,9 @@ def loop_device_cleanup(password):
             run_ssh('umount /dev/loop0', throw=False, password=password)
 
     for loop in run_ssh('losetup', password=password).splitlines():
-        if 'loop0' in loop:
-            run_ssh('losetup -d /dev/loop0', throw=False, password=password)
+        for i in range(0, 5):
+            if 'loop{0}'.format(i) in loop:
+                run_ssh('losetup -d /dev/loop{0}'.format(i), throw=False, password=password)
 
     run_ssh('losetup', password=password)
 
