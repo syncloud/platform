d2817936b7da5d11c715397c9463356fbeb3f0fe
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-10-17 00:09:06 -0400

    Added home folder as a parameter

--- src/syncloud_platform/gaplib/linux.py
+++ src/syncloud_platform/gaplib/linux.py
@@ -4,14 +4,15 @@ from os import environ
 import massedit
 
 
-def useradd(user, create_home=False):
+def useradd(user, home_folder=None):
     try:
         pwd.getpwnam(user)
         return 'user {0} exists'.format(user)
     except KeyError:
         options = '-r -s /bin/false'
-        if create_home:
-            options = '-m ' + options
+        if home_folder:
+            home_folder_options = '-m -d {0}'.format(home_folder)
+            options = home_folder_options + ' ' + options
         command_line = '/usr/sbin/useradd {0} {1}'.format(options, user)
         return check_output(command_line, shell=True)
 
