a4d5aaa90182be1cf67dc29252d0acb01694e4a1
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-11-05 15:19:19 -0400

    Added ability to run new hooks from hooks folder

--- src/syncloud_platform/events.py
+++ src/syncloud_platform/events.py
@@ -1,15 +1,29 @@
 import traceback
-from os import path
-from os.path import join
+from os.path import join, isfile, isdir
 from syncloud_app import logger
 from syncloud_platform.gaplib.scripts import run_script
 
 
-def run_hook_script(platform_config, hook_script, app_id):
+action_to_old_script = {
+    'post-install': 'post-install',
+    'pre-remove': 'pre-remove',
+    'storage-change': 'on_disk_change.py',
+    'access-change': 'on_domain_change.py'
+}
+
+
+def get_script_path(apps_root, app_id, action):
+    hooks_path = join(apps_root, 'hooks')
+    if isdir(hooks_path):
+        return join(hooks_path, action+'.py')
+    else:
+        hook_script = action_to_old_script[action]
+        return join(apps_root, app_id, 'bin', hook_script)
+
+
+def try_run_script(app_event_script):
     log = logger.get_logger('events')
-    apps_root = platform_config.apps_root()
-    app_event_script = join(apps_root, app_id, 'bin', hook_script)
-    if path.isfile(app_event_script):
+    if isfile(app_event_script):
         log.info('executing {0}'.format(app_event_script))
         try:
             run_script(app_event_script)
@@ -20,18 +34,24 @@ def run_hook_script(platform_config, hook_script, app_id):
         log.info('{0} not found'.format(app_event_script))
 
 
+def run_hook_script(platform_config, action, app_id):
+    apps_root = platform_config.apps_root()
+    app_event_script = get_script_path(apps_root, app_id, action)
+    try_run_script(app_event_script)
+
+
 class EventTrigger:
     def __init__(self, sam, platform_config):
         self.sam = sam
         self.platform_config = platform_config
 
     def trigger_app_event_disk(self):
-        self.__trigger_app_event('on_disk_change.py')
+        self.__trigger_app_event('storage-change')
 
     def trigger_app_event_domain(self):
-        self.__trigger_app_event('on_domain_change.py')
+        self.__trigger_app_event('access-change')
 
-    def __trigger_app_event(self, event_script):
+    def __trigger_app_event(self, action):
         for app in self.sam.installed_all_apps():
             app_id = app.app.id
-            run_hook_script(self.platform_config, event_script, app_id)
+            run_hook_script(self.platform_config, action, app_id)
