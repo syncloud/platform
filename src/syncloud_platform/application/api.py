c0aeac902222201e3622fa30637b4b72b9853230
Boris Rybalkin <ribalkin@gmail.com>
2016-11-20 14:01:28 +0000

    do not use injector in app paths as it is too early

--- src/syncloud_platform/application/api.py
+++ src/syncloud_platform/application/api.py
@@ -1,10 +1,10 @@
 from syncloud_platform.config.config import PlatformConfig
 from syncloud_platform.application.apppaths import AppPaths
 from syncloud_platform.application.appsetup import AppSetup
-from syncloud_platform.injector import get_injector
+from syncloud_platform.injector import get_injector, PLATFORM_CONFIG_DIR
 
 def get_app_paths(app_name, config_path=None):
-    config = get_injector(config_path).platform_config()
+    config = PlatformConfig(PLATFORM_CONFIG_DIR)
     return AppPaths(app_name, config)
 
 def get_app_setup(app_name):
