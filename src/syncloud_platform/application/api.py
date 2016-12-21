ee98bd6d33ad647dfc963ed3393ff0267a8155b1
Boris Rybalkin <ribalkin@gmail.com>
2016-12-21 19:49:34 +0000

    fix api to use systemd instance

--- src/syncloud_platform/application/api.py
+++ src/syncloud_platform/application/api.py
@@ -3,12 +3,15 @@ from syncloud_platform.application.apppaths import AppPaths
 from syncloud_platform.application.appsetup import AppSetup
 from syncloud_platform.injector import get_injector, PLATFORM_CONFIG_DIR
 
+
 def get_app_paths(app_name, config_path=None):
     config = PlatformConfig(PLATFORM_CONFIG_DIR)
     return AppPaths(app_name, config)
 
+
 def get_app_setup(app_name):
     app_paths = get_app_paths(app_name)
     injector = get_injector()
-    app_setup = AppSetup(app_name, app_paths, injector.nginx, injector.hardware, injector.info, injector.device, injector.user_platform_config)
+    app_setup = AppSetup(app_name, app_paths, injector.nginx, injector.hardware, injector.info,
+                         injector.device, injector.user_platform_config, injector.systemctl)
     return app_setup
--- src/syncloud_platform/application/appsetup.py
+++ src/syncloud_platform/application/appsetup.py
@@ -2,7 +2,7 @@ from syncloud_platform.control import systemctl
 
 class AppSetup:
 
-    def __init__(self, app_name, app_paths, nginx, storage, device_info, device, user_platform_config):
+    def __init__(self, app_name, app_paths, nginx, storage, device_info, device, user_platform_config, systemctl):
         self.app_name = app_name
         self.app_paths = app_paths
         self.nginx = nginx
@@ -10,6 +10,7 @@ class AppSetup:
         self.device_info = device_info
         self.device = device
         self.user_platform_config = user_platform_config
+        self.systemctl = systemctl
 
     def get_install_dir(self):
         return self.app_paths.get_install_dir()
@@ -45,22 +46,22 @@ class AppSetup:
         self.device.remove_port(local_port, protocol)
 
     def add_service(self, service_name):
-        systemctl.add_service(self.app_name, service_name)
+        self.systemctl.add_service(self.app_name, service_name)
 
     def remove_service(self, service_name):
-        systemctl.remove_service(service_name)
+        self.systemctl.remove_service(service_name)
 
     def restart_service(self, service_name):
-        systemctl.restart_service(service_name)
+        self.systemctl.restart_service(service_name)
 
     def reload_service(self, service_name):
-        systemctl.reload_service(service_name)
+        self.systemctl.reload_service(service_name)
 
     def start_service(self, service_name):
-        systemctl.start_service(service_name)
+        self.systemctl.start_service(service_name)
 
     def stop_service(self, service_name):
-        systemctl.stop_service(service_name)
+        self.systemctl.stop_service(service_name)
 
     def redirect_email(self):
         return self.user_platform_config.get_user_email()
