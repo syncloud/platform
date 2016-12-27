516c65f91915cd140fbc93838b6b1864a47389d8
Boris Rybalkin <ribalkin@gmail.com>
2016-12-27 18:35:25 +0000

    LDAP: fix constant

--- src/syncloud_platform/auth/ldapauth.py
+++ src/syncloud_platform/auth/ldapauth.py
@@ -26,7 +26,7 @@ class LdapAuth:
         self.user_conf_dir = join(self.config.data_dir(), ldap_user_conf_dir)
 
     def installed(self):
-        return os.path.isdir(join(self.config.data_dir(), self.ldap_user_conf_dir))
+        return os.path.isdir(join(self.config.data_dir(), ldap_user_conf_dir))
 
     def init(self):
         if self.installed():
