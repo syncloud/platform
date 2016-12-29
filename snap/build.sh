439a5b6b5c7d6501a597ff3c988ac179a94baaea
Boris Rybalkin <ribalkin@gmail.com>
2016-12-29 18:46:15 +0000

    snap: set version

--- snap/build.sh
+++ snap/build.sh
@@ -9,10 +9,12 @@ if [[ -z "$1" || -z "$2" ]]; then
     exit 1
 fi
 
+VERSION=$2
 cd ${DIR}/..
-./build.sh $1 $2
+./build.sh $1 $VERSION
 
 cd snap
+sed 's/VERSION/$VERSION/g' -i snapcraft.yaml
 snapcraft clean
 rm -rf *.snap
 snapcraft prime
