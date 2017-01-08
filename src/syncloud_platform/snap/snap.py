ffa5043b14aece8106265bc19fd36bc7462265fc
Boris Rybalkin <ribalkin@gmail.com>
2017-01-08 22:44:50 +0000

    fix snap requests

--- src/syncloud_platform/snap/snap.py
+++ src/syncloud_platform/snap/snap.py
@@ -35,25 +35,25 @@ class Snap:
 
     def user_apps(self):
         session = requests_unixsocket.Session()
-        response = session..get('{0}/v2/snaps'.format(SOCKET))
+        response = session.get('{0}/v2/snaps'.format(SOCKET))
         self.logger.info(response)
         return response
 
     def installed_user_apps(self):
         session = requests_unixsocket.Session()
-        response = session..get('{0}/v2/snaps'.format(SOCKET))
+        response = session.get('{0}/v2/snaps'.format(SOCKET))
         self.logger.info(response)
         return response
 
     def installed_all_apps(self):
         session = requests_unixsocket.Session()
-        response = session..get('{0}/v2/snaps'.format(SOCKET))
+        response = session.get('{0}/v2/snaps'.format(SOCKET))
         self.logger.info(response)
         return response
 
     def get_app(self, app_id):
         session = requests_unixsocket.Session()
-        response = session..get('{0}/v2/snaps/{1}'.format(SOCKET, app_od))
+        response = session.get('{0}/v2/snaps/{1}'.format(SOCKET, app_id))
         self.logger.info(response)
         return response
 
