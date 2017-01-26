32f1f2d64e43fa61738ffe901fbd343e8d1c5938
Boris Rybalkin <ribalkin@gmail.com>
2017-01-26 06:57:27 +0000

    test hook logging

--- integration/platform/hooks/storage-change.py
+++ integration/platform/hooks/storage-change.py
@@ -1,9 +1,16 @@
 #!/opt/app/platform/python/bin/python
 from syncloud_platform.application import api
 import os
+from syncloud_app import logger
 
-app = api.get_app_setup('platform')
-storage_dir = os.path.realpath(app.init_storage('platform'))
+log = logger.get_logger('test_hook')
 
-with open('/tmp/on_disk_change.log', 'w+') as f:
-    f.write(storage_dir)
+try:
+
+    app = api.get_app_setup('platform')
+    storage_dir = os.path.realpath(app.init_storage('platform'))
+
+    with open('/tmp/on_disk_change.log', 'w+') as f:
+        f.write(storage_dir)
+ except Exception, e:
+    log.error('error {0}'.format(e.message))
