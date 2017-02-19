c20812701ea7e974c9c7a35fba98b21a83c83dc6
Boris Rybalkin <ribalkin@gmail.com>
2017-02-19 22:48:26 +0000

    use dict

--- src/syncloud_platform/network/network.py
+++ src/syncloud_platform/network/network.py
@@ -1,11 +1,5 @@
 import netifaces
 
-class Address:
-    def __init__(self, card, ipv4, ipv6):
-        self.card=card
-        self.ipv4=ipv4
-        self.ipv6=ipv6
-
 class Network:
     def interfaces(self):
         ifaces=netifaces.interfaces()
@@ -15,7 +9,7 @@ class Network:
         addrs=netifaces.ifaddresses(iface)
         ipv4=self.__addr(addrs[netifaces.AF_INET])
         ipv6=self.__addr(addrs[netifaces.AF_INET6])
-        return Address(iface, ipv4, ipv6)
+        return dict(iface=iface, ipv4=ipv4, ipv6=ipv6)
 
     def __addr(self, info):
         if 'addr' in info:
