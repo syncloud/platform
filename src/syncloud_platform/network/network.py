1ba8b4d299ed0a9162f7fa5833caa99afcbbf26a
Boris Rybalkin <ribalkin@gmail.com>
2017-02-19 23:25:25 +0000

    network logs

--- src/syncloud_platform/network/network.py
+++ src/syncloud_platform/network/network.py
@@ -1,6 +1,10 @@
 import netifaces
+from syncloud_app import logger
 
 class Network:
+    def __init__(self):
+        self.log = logger.get_logger('network')
+
     def interfaces(self):
         ifaces=netifaces.interfaces()
         return [self.__convert(iface) for iface in ifaces]
@@ -8,7 +12,9 @@ class Network:
     def __convert(self, iface):
         addrs=netifaces.ifaddresses(iface)
         ipv4=self.__addr(addrs[netifaces.AF_INET])
+        self.log.info(addrs[netifaces.AF_INET])
         ipv6=self.__addr(addrs[netifaces.AF_INET6])
+        self.log.info(addrs[netifaces.AF_INET6])
         return dict(iface=iface, ipv4=ipv4, ipv6=ipv6)
 
     def __addr(self, info):
