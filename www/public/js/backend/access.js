ecd7c1e7ce9e1ad2056873f9ef7f585136ec8292
Boris Rybalkin <ribalkin@gmail.com>
2017-02-20 21:47:43 +0000

    network js backend

--- www/public/js/backend/access.js
+++ www/public/js/backend/access.js
@@ -23,6 +23,29 @@ var backend = {
             });
     },
 
+    network_interfaces: function(parameters) {
+        $.get('/rest/access/network_interfaces')
+            .done(function (data) {
+                if (parameters.hasOwnProperty("done")) {
+                    parameters.done(data);
+                }
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                var error = null;
+                if (xhr.hasOwnProperty('responseJSON')) {
+                    var error = xhr.responseJSON;
+                }
+                if (parameters.hasOwnProperty("fail")) {
+                    parameters.fail(xhr.status, error);
+                }
+            })
+            .always(function() {
+                if (parameters.hasOwnProperty("always")) {
+                    parameters.always();
+                }
+            });
+    },
+
     external_access: function(parameters) {
         var state = parameters.state;
         $.get('/rest/access/set_external_access?external_access=' + state)
