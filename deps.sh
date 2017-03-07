a3ca4e4b6a615f79902a4aa264a5d4e5e7eed6c9
Boris Rybalkin <ribalkin@gmail.com>
2017-03-07 18:56:28 +0000

    arm phantomjs binary

--- deps.sh
+++ deps.sh
@@ -5,7 +5,7 @@ sudo gem install jekyll
 
 ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
 if [ $ARCH == "amd64" ]; then
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  wget https://github.com/fg2it/phantomjs-on-raspberry/releases/download/v2.1.1-wheezy-jessie-armv6/phantomjs
   tar xjvf https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
   cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
 else
