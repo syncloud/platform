edaee68b952d9f50b536cc6fef14d554c0d88032
Boris Rybalkin <ribalkin@gmail.com>
2017-03-11 15:40:24 +0000

    settings sam upgrade js test + refactoring

--- test/js/settings.js
+++ test/js/settings.js
@@ -14,6 +14,14 @@ QUnit.test( "settings platform upgrade", function( assert ) {
   assert.deepEqual( true, true);
 });
 
+QUnit.test( "settings sam upgrade", function( assert ) {
+
+  backend.async = false;
+  sam_upgrade(function(data) {}, function() {}, function(a, b, c) {});
+
+  assert.deepEqual( true, true);
+});
+
 QUnit.test( "settings boot disk extend", function( assert ) {
 
   backend.async = false;
--- www/public/js/settings.js
+++ www/public/js/settings.js
@@ -39,3 +39,18 @@ function boot_extend(on_complete, on_always, on_error) {
     }, on_error);
 
 }
+
+function sam_upgrade(on_complete, on_always, on_error) {
+
+    backend.sam_upgrade(function (data) {
+        check_for_service_error(data, function () {
+            run_after_sam_is_complete(function () {
+                 backend.get_versions(
+                     on_complete, 
+                     on_always, 
+                     on_error);
+                 }, on_error);
+        }, on_error);
+    }, on_error);
+    
+}
