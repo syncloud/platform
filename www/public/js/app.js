455cc0ea90ed35495a45507ed287294d82de814a
Boris Rybalkin <ribalkin@gmail.com>
2017-03-25 16:33:00 +0000

    js app action: fix parameter order

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -1,15 +1,10 @@
 function run_app_action(app_id, action, on_complete, on_error) {
-        
-        
-            backend.app_action(app_id, action, function (data) {
-                    check_for_service_error(data, function () {
-                            run_after_sam_is_complete(
-                                    on_complete,
-                                    on_error
-                                );
-                            },
-                            on_error)
-                    },
-                    on_error
-            );
+
+    backend.app_action(app_id, action, function (data) {
+        check_for_service_error(data, function () {
+            run_after_sam_is_complete(
+                on_complete,
+                on_error);
+        }, on_error)
+    }, on_error);
 }
--- www/public/js/backend/app.js
+++ www/public/js/backend/app.js
@@ -1,7 +1,7 @@
-backend.load_app = function(app_id, on_complete, on_error) {
-        $.get( '/rest/app', {app_id: app_id}).done(on_complete).fail(on_error);
-    };
-    
-backend.app_action = function(action, app_id, on_always, on_error) {
-        $.get("/rest/"+action, {app_id: app_id}).always(on_always).fail(on_error);
-    };
+backend.load_app = function (app_id, on_complete, on_error) {
+    $.get('/rest/app', {app_id: app_id}).done(on_complete).fail(on_error);
+};
+
+backend.app_action = function (app_id, action, on_always, on_error) {
+    $.get("/rest/" + action, {app_id: app_id}).always(on_always).fail(on_error);
+};
