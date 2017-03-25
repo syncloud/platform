e2d94ab149023623ce2a751a68fa2484fd65efe8
Boris Rybalkin <ribalkin@gmail.com>
2017-03-25 10:48:22 +0000

    install coin

--- deps.sh
+++ deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 apt-get install -y squashfs-tools dpkg-dev
+pip install coin
 ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
 if [ $ARCH == "amd64" ]; then
   wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
