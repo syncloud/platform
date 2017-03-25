759ae274407af353ad8021cbb566810e7e2dc5be
Boris Rybalkin <ribalkin@gmail.com>
2017-03-25 15:31:09 +0000

    libffi dep

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-apt-get install -y squashfs-tools dpkg-dev python-dev libsasl2-dev libldap2-dev libssl-dev
+apt-get install -y squashfs-tools dpkg-dev python-dev libsasl2-dev libldap2-dev libssl-dev libffi-dev
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install coin
