b18ca00d1da26753a4f90f6ed24d47e9bf104430
Boris Rybalkin <ribalkin@gmail.com>
2017-04-16 21:39:57 +0100

    fix deps

--- deps.sh
+++ deps.sh
@@ -4,8 +4,8 @@ apt-get install -y squashfs-tools dpkg-dev python-dev libsasl2-dev libldap2-dev 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install coin
-ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
-if [ $ARCH == "amd64" ]; then
+ARCH=$(uname -m)
+if [ $ARCH == "x86_64" ]; then
   wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
   tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
   cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
