2d492e05a694688473c2283d15c9e5373a62429e
Boris Rybalkin <ribalkin@gmail.com>
2017-05-25 23:17:16 +0100

    run integration

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -6,8 +6,8 @@ export TMPDIR=/tmp
 export TMP=/tmp
 export DEBIAN_FRONTEND=noninteractive
 
-if [ "$#" -lt 8 ]; then
-    echo "usage $0 redirect_user redirect_password redirect_domain app_archive_path installer_version release [all|test_suite] [sam|snapd]"
+if [ "$#" -lt 9 ]; then
+    echo "usage $0 redirect_user redirect_password redirect_domain app_archive_path installer_version release [all|test_suite] [sam|snapd] device_host"
     exit 1
 fi
 
@@ -16,6 +16,7 @@ INSTALLER_VERSION=$5
 RELEASE=$6
 TEST=$7
 INSTALLER=$8
+DEVICE_HOST=$9
 
 GECKODRIVER=0.14.0
 FIREFOX=50.0
@@ -29,11 +30,21 @@ else
 fi
 
 cd ${DIR}
-./docker.sh ${RELEASE}
+#./docker.sh ${RELEASE}
 
-sshpass -p syncloud scp -o StrictHostKeyChecking=no -P 2222 install-${INSTALLER}.sh root@localhost:/installer.sh
+ssh-keygen -f "/root/.ssh/known_hosts" -R [${DEVICE_HOST}]
+sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} date
+while test $? -gt 0
+do
+  sleep 3
+  echo "Waiting for SSH ..."
+  sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} date
+done
 
-sshpass -p syncloud ssh -o StrictHostKeyChecking=no -p 2222 root@localhost /installer.sh ${INSTALLER_VERSION} ${RELEASE}
+
+sshpass -p syncloud scp -o StrictHostKeyChecking=no install-${INSTALLER}.sh root@${DEVICE_HOST}:/installer.sh
+
+sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} /installer.sh ${INSTALLER_VERSION} ${RELEASE}
 
 apt-get install -y sshpass xvfb
 
