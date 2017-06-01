bf9300bbad500983be907d6033d1005e50e0d7df
Boris Rybalkin <ribalkin@gmail.com>
2017-06-01 22:04:30 +0100

    fix pip deps

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -58,10 +58,10 @@ sshpass -p syncloud scp -o StrictHostKeyChecking=no install-${INSTALLER}.sh root
 sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@${DEVICE_HOST} /installer.sh ${INSTALLER_VERSION} ${RELEASE}
 
 apt-get install -y sshpass xvfb
+pip2 install -r ${DIR}/../src/dev_requirements.txt
 
 coin --to ${DIR} raw --subfolder geckodriver https://github.com/mozilla/geckodriver/releases/download/v${GECKODRIVER}/geckodriver-v${GECKODRIVER}-linux64.tar.gz
 coin --to ${DIR} raw https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX}/linux-x86_64/en-US/firefox-${FIREFOX}.tar.bz2
 curl https://raw.githubusercontent.com/mguillem/JSErrorCollector/master/dist/JSErrorCollector.xpi -o  JSErrorCollector.xpi
 
-pip2 install -r ${DIR}/../src/dev_requirements.txt
 xvfb-run -l --server-args="-screen 0, 1024x4096x24" py.test -x -s ${TEST_SUITE} --email=$1 --password=$2 --domain=$3 --app-archive-path=${APP_ARCHIVE_PATH} --installer=${INSTALLER}
