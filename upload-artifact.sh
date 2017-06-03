a0670b37f0e94d3d2dc04bd467a582717d4462a1
Boris Rybalkin <ribalkin@gmail.com>
2017-06-03 13:24:44 +0000

    upload artifact [skip ci]

---
+++ upload-artifact.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if [ -z "$ARTIFACT_SSH_KEY" ]; then
+  echo "ARTIFACT_SSH_KEY must be set"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "usage $0 src dst"
+  exit 1
+fi
+
+echo "$ARTIFACT_SSH_KEY" | base64 --decode > artifact_ssh.key
+chmod 600 artifact_ssh.key
+scp -r -oStrictHostKeyChecking=no -i artifact_ssh.key $1 artifact@artifact.syncloud.org:/home/artifact/repo/ci/platform/$2
