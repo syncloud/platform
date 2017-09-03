ee00127828126ff1d17e9b7d50e10d57dc3d31cc
Boris Rybalkin <ribalkin@gmail.com>
2017-09-03 14:20:10 +0100

    fix device host

--- integration/verify.py
+++ integration/verify.py
@@ -161,7 +161,7 @@ def test_platform_rest(device_host):
     response = session.get('http://{0}'.format(device_host), timeout=60)
     assert response.status_code == 200
 
-def test_app_unix_socket(app_dir):
+def test_app_unix_socket(app_dir, device_host):
     run_scp('{0}/nginx.app.test.conf root@{1}:/'.format(DIR, device_host), throw=False, password=LOGS_SSH_PASSWORD)
     run_ssh(device_host, '{0}/nginx/sbin/nginx -c /nginx.app.test.conf -g \'error_log {1}/log/nginx_app_error.log warn;\''.format(app_dir, DIR), password=DEVICE_PASSWORD)
     response = requests.get('http://unix_socket_app.{0}'.format(device_host), timeout=60)
