a6221daa9ef142f54755e9a15ca3acaee7d526d2
Boris Rybalkin <ribalkin@gmail.com>
2017-09-19 20:43:13 +0100

    tabs experiments

--- www/internal/js/backend.static/activate.js
+++ www/internal/js/backend.static/activate.js
@@ -8,7 +8,7 @@ var backend = {
     activate: function(parameters) {
         setTimeout(function() {
             parameters.always();
-            parameters.fail(400, {message: parameters.values})
+            parameters.fail(400, {message: parameters.values.domain_type})
         }, 2000);
     },
     login: function(name, password) {
