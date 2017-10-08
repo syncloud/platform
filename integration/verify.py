8736ea9ae86298eae1ff21447b82bfccafd81e4a
Boris Rybalkin <ribalkin@gmail.com>
2017-10-08 10:38:27 +0100

    retry losetup check

--- integration/verify.py
+++ integration/verify.py
@@ -360,11 +360,12 @@ def loop_device(device_host):
     run_ssh(device_host, 'sync', password=DEVICE_PASSWORD)
     run_ssh(device_host, 'ls -la {0}'.format(dev_file), password=DEVICE_PASSWORD)
     loop = run_ssh(device_host, 'losetup -f --show {0}'.format(dev_file), password=DEVICE_PASSWORD)
+    run_ssh(device_host, 'losetup', password=DEVICE_PASSWORD)
+    run_ssh(device_host, 'losetup -j {0} | grep {0}'.format(dev_file), password=DEVICE_PASSWORD, retries=3)
     run_ssh(device_host, 'file -s {0}'.format(loop), password=DEVICE_PASSWORD)
     run_ssh(device_host, 'sync', password=DEVICE_PASSWORD)
     run_ssh(device_host, 'partprobe {0}'.format(loop), password=DEVICE_PASSWORD, retries=3)
-    run_ssh(device_host, 'losetup -j {0}'.format(dev_file), password=DEVICE_PASSWORD)
-    yield loop
+     yield loop
 
     loop_device_cleanup(device_host, dev_file, password=DEVICE_PASSWORD)
 
