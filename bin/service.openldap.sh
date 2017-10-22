d83bb8e8ce7b68289a150d1d10413747422e2233
Boris Rybalkin <ribalkin@gmail.com>
2017-10-22 11:20:26 +0100

    fix snapd openldap service

--- bin/service.openldap.sh
+++ bin/service.openldap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-
+expprt LD_LIBRARY_PATH=$DIR/openldap/lib
 $DIR/openldap/libexec/slapd -h ldap://127.0.0.1:389 -F ${SNAP_COMMON}/slapd.d
 
 
