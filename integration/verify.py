d628c028bd08aa89ee2de9dd1f8de3778ccbd669
Boris Rybalkin <ribalkin@gmail.com>
2017-12-06 09:38:01 +0000

    fix test

--- integration/verify.py
+++ integration/verify.py
@@ -180,7 +180,9 @@ def test_app_unix_socket(app_dir, data_dir, app_data_dir, main_domain):
 
 
 def test_api_rest_socket(app_dir, data_dir, app_data_dir, main_domain):
-    socket_file = '/opt/data/platform/api.socket'
+    run_ssh(main_domain, 'ls -la {0}'.format(data_dir), password=DEVICE_PASSWORD)
+    
+    socket_file = '{0}/api.socket'.format(data_dir)
     run_ssh(main_domain, 'socat TCP-LISTEN:1234,reuseaddr,fork UNIX-CLIENT:{0}'.format(socket_file), password=DEVICE_PASSWORD)
     
     url = "http://{0}:1234".format(main_domain)
