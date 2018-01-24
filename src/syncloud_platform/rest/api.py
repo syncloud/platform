fdbab5717c993b061c3d30dca11dd6c53377ba7a
Boris Rybalkin <ribalkin@gmail.com>
2018-01-24 14:25:12 +0000

    rest api: init storage

--- src/syncloud_platform/rest/api.py
+++ src/syncloud_platform/rest/api.py
@@ -29,6 +29,15 @@ def app_url():
     url = get_app_setup(app_name).app_url()
     return jsonify(success=True, message='', data=url), 200
 
+
+@app.route("/app/init_storage", methods=["POST"])
+def init_storage():
+    app_name = request.args['app_name']
+    user_name = request.args['user_name']
+    app_storage_dir = get_app_setup(app_name).init_storage(user_name)
+    return jsonify(success=True, message='', data=app_storage_dir), 200
+
+
 @app.errorhandler(Exception)
 def handle_exception(error):
     status_code = 500
