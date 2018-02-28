c5f6aeff97fda9ffec841b5b1a5f6d192ef8382b
Boris Rybalkin <ribalkin@gmail.com>
2018-02-28 18:26:37 +0000

    fix port drill port assert

--- src/test/insider/test_port_drill.py
+++ src/test/insider/test_port_drill.py
@@ -4,7 +4,7 @@ from test.insider.helpers import get_port_config, get_user_platform_config
 
 
 def test_port_drill():
-    port_config = get_port_config([Port(80, 80, 'TCP'), Port(81, 81, 'TCP')])
+    port_config = get_port_config([Port(80, 81, 'TCP')])
     port_drill = PortDrill(port_config, MockPortMapper(external_ip='192.167.44.52'), MockPortProber())
     port_drill.sync()
     mapping = port_drill.get(80, 'TCP')
