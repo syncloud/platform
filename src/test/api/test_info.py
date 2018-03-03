6f1e5e14259a1f394c02ec05a5732da8ad7df049
Boris Rybalkin <ribalkin@gmail.com>
2018-03-03 22:09:13 +0000

    fix url test

--- src/test/api/test_info.py
+++ src/test/api/test_info.py
@@ -1,17 +1,9 @@
 from syncloud_platform.insider.device_info import construct_url
 
 
-def test_url_no_protocol():
-    assert construct_url(None, 80, 'domain.tld') == 'http://domain.tld'
-
-
 def test_url_standard_port():
-    assert construct_url('https', 443, 'domain.tld') == 'https://domain.tld'
+    assert construct_url(443, 'domain.tld') == 'https://domain.tld'
 
 
 def test_url_non_standard_port():
-    assert construct_url('https', 444, 'domain.tld') == 'https://domain.tld:444'
-
-
-def test_url_sub_domain():
-    assert construct_url('https', 444, 'domain.tld', 'app') == 'https://app.domain.tld:444'
+    assert construct_url(444, 'domain.tld') == 'https://domain.tld:444'
