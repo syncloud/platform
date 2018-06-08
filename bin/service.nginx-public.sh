f4a17c057723e46f32bd21e394af0dcc8678e2a3
Boris Rybalkin <ribalkin@gmail.com>
2018-06-08 16:43:47 +0100

    try to fix reload issue

--- bin/service.nginx-public.sh
+++ bin/service.nginx-public.sh
@@ -12,16 +12,16 @@ pre-start)
     ${DIR}/nginx/sbin/nginx -t -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -g 'error_log '${SNAP_COMMON}'/log/nginx_public_error.log warn;'
     ;;
 start)
-    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf
+    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -g 'error_log '${SNAP_COMMON}'/log/nginx_public_error.log warn;'
     ;;
 post-start)
     timeout 5 /bin/bash -c 'until echo > /dev/tcp/localhost/80; do sleep 1; done'
     ;;
 reload)
-    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -s reload
+    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -s reload -g 'error_log '${SNAP_COMMON}'/log/nginx_public_error.log warn;'
     ;;
 stop)
-    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -s stop
+    $DIR/nginx/sbin/nginx -c ${SNAP_COMMON}/config.runtime/nginx/nginx.conf -s stop -g 'error_log '${SNAP_COMMON}'/log/nginx_public_error.log warn;'
     ;;
 *)
     echo "not valid command"
