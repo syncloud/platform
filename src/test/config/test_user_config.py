f01763cd05a039e61438ea2355958d35afcc5072
Boris Rybalkin <ribalkin@gmail.com>
2018-10-13 09:41:13 +0100

    fix test

--- src/test/config/test_user_config.py
+++ src/test/config/test_user_config.py
@@ -3,8 +3,9 @@ from test.insider.helpers import temp_file
 
 
 def test_domain():
-    config = PlatformUserConfig(config_file=temp_file())
-
+    config = PlatformUserConfig(temp_file())
+    config.init_user_config()
+    
     config.update_redirect('syncloud.it', 'http://api.syncloud.it')
     assert 'syncloud.it' == config.get_redirect_domain()
     assert 'http://api.syncloud.it' == config.get_redirect_api_url()
