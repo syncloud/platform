308660bf003efe2465985e9d9c8d52fc209c7bc4
Boris Rybalkin <ribalkin@gmail.com>
2019-01-14 20:13:59 +0000

    backup script

---
+++ bin/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+
+if [[ -z "$1" ]]; then
+    echo "usage $0 app"
+    exit 1
+fi
+
+APP=$1
+
+BACKUP_NAME=`date +"%Y%m%d_%H%M%S"`
+BASE_DIR=/data/platform/backup/${APP}
+BACKUP_DIR=${BASE_DIR}/${BACKUP_NAME}
+
+mkdir -p ${BACKUP_DIR}
+
+snap stop $APP
+cp -r /var/snap/$APP/current/ ${BACKUP_DIR}/
+cp -r /var/snap/$APP/common/ ${BACKUP_DIR}/
+snap start $APP
+tar czf -C ${BASE_DIR} ${BACKUP_NAME}.tar.gz ${BACKUP_NAME}
+rm -rf ${BACKUP_DIR}
