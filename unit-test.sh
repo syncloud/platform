4247be4907f3d28f93fed55163bc4d5d47d39df8
Boris Rybalkin <ribalkin@gmail.com>
2019-01-25 09:53:37 +0000

    debug pip

--- unit-test.sh
+++ unit-test.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 NAME="platform"
 BUILD_DIR=${DIR}/build/${NAME}
-
+${BUILD_DIR}/python/bin/pip --help
 ${BUILD_DIR}/python/bin/pip install -r ${DIR}/dev_requirements.txt
 
 cd ${DIR}/src
