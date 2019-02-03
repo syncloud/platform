339cafabec9e97dd51ad3f09fbf4a0e8088992d3
Boris Rybalkin <ribalkin@gmail.com>
2019-02-03 13:52:33 +0000

    bring go compiler and start go backend

---
+++ backend/backend.go
@@ -0,0 +1,7 @@
+package syncloud.platform
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, world.")
+}
--- build.sh
+++ build.sh
@@ -10,13 +10,19 @@ fi
 NAME=$1
 ARCH=$(uname -m)
 VERSION=$2
+GO_VERSION=1.11.5
 
 cd ${DIR}
 
 BUILD_DIR=${DIR}/build/${NAME}
+GOROOT=${DIR}/go
+PYTHON_DIR=${BUILD_DIR}/python
+export PATH=${PYTHON_DIR}/bin:$GOROOT/bin:$PATH
 SNAP_DIR=${DIR}/build/snap
 rm -rf build
 mkdir -p ${BUILD_DIR}
+
+cp -r ${DIR}/bin ${BUILD_DIR}
 
 DOWNLOAD_URL=http://artifact.syncloud.org/3rdparty
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/nginx-${ARCH}.tar.gz
@@ -25,20 +31,36 @@ coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/openldap-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/openssl-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/python-${ARCH}.tar.gz
 
-PYTHON_DIR=${BUILD_DIR}/python
+GO_ARCH=armv6l
+if [[ ${ARCH} == "x86_64" ]]; then
+    GO_ARCH=amd64
+fi
+
+wget https://dl.google.com/go/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz --progress dot:giga
+tar xf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
+
+go version
+mkdir -p ${BUILD_DIR}/go/src/syncloud/platform
+cp -r ${DIR}/backend/. ${BUILD_DIR}/go/src/syncloud/platform
+export GOPATH=${BUILD_DIR}/go
+cd ${BUILD_DIR}/go/src/syncloud/platform
+go build
+./backend
+cp backend ${BUILD_DIR}/bin
+rm -rf ${BUILD_DIR}/go
+
 export CPPFLAGS=-I${PYTHON_DIR}/include
 export LDFLAGS=-L${PYTHON_DIR}/lib
 export LD_LIBRARY_PATH=${PYTHON_DIR}/lib
 
-${PYTHON_DIR}/bin/pip install -r ${DIR}/requirements.txt
+pip install -r ${DIR}/requirements.txt
 
-cd src
+cd ${DIR}/src
 rm -f version
 echo ${VERSION} >> version
 ${PYTHON_DIR}/bin/python setup.py install
 cd ..
 
-cp -r ${DIR}/bin ${BUILD_DIR}
 cp -r ${DIR}/config ${BUILD_DIR}/config.templates
 cp -r ${DIR}/www ${BUILD_DIR}
 
@@ -49,6 +71,7 @@ echo ${VERSION} >> ${BUILD_DIR}/META/version
 echo "snapping"
 ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
 rm -rf ${DIR}/*.snap
+
 mkdir ${SNAP_DIR}
 cp -r ${BUILD_DIR}/* ${SNAP_DIR}/
 cp -r ${DIR}/snap/meta ${SNAP_DIR}/
