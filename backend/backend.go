099a11f08611d0c0a17b0a3927394d1629421d11
Boris Rybalkin <ribalkin@gmail.com>
2019-02-03 16:39:33 +0000

    unix socket listen

--- backend/backend.go
+++ backend/backend.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "request %s", req.URL.Path[1:])
+}
 
 func main() {
-    fmt.Println("Hello, world.")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage:", os.Args[0], "/path.sock")
+		return
+	}
+
+	fmt.Println("Starting backend")
+
+	os.Remove(os.Args[1])
+	http.HandleFunc("/", hello)
+	server := http.Server{}
+
+	unixListener, err := net.Listen("unix", os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	server.Serve(unixListener)
 }
