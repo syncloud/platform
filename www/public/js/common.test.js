d1853ada7b4e99a3f5fa1f7e94eaf64b9962a692
Boris Rybalkin <ribalkin@gmail.com>
2019-03-03 15:13:51 +0000

    fix test

--- www/public/js/common.test.js
+++ www/public/js/common.test.js
@@ -4,11 +4,12 @@ test('job status', () => {
     var checker_count = 0;
     var checker_on_complete;
     var checker_job;
+    var checker_on_error;
     var checker = function (job, on_complete, on_error) {
         checker_count += 1;
         checker_job = job;
         checker_on_complete = on_complete;
-        this.on_error = on_error;
+        checker_on_error = on_error;
     };
     
     Common.run_after_job_is_complete(checker, function(func, timeout) { func(); }, function() {}, function(a, b, c) {}, 'test');
