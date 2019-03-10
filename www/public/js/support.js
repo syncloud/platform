595595768ecac3464065077f15d07106ac388487
Boris Rybalkin <ribalkin@gmail.com>
2019-03-10 10:09:09 +0000

    migrating bootstrap switch css settings

--- www/public/js/support.js
+++ www/public/js/support.js
@@ -1,11 +1,11 @@
 import * as _ from 'underscore';
 import $ from 'jquery';
 import jQuery from 'jquery';
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-switch';
-import 'bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css';
+//import 'bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css';
+import '../css/bootstrap-switch.css';
 import 'font-awesome/css/font-awesome.css'
 import '../css/site.css'
 
