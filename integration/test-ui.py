a2073405ac8cfd5f459cd6aea7f80c889ca6f7d5
Boris Rybalkin <ribalkin@gmail.com>
2019-03-19 21:58:46 +0000

    fix screenshots import

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -1,21 +1,19 @@
-import os
-import shutil
-from os.path import dirname, join, exists
-import pytest
 import time
+from os.path import dirname, join
+
+import pytest
 import requests
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from syncloudlib.integration.screenshots import screenshots
 
 DIR = dirname(__file__)
 LOG_DIR = join(DIR, 'log')
 DEVICE_USER = 'user'
 DEVICE_PASSWORD = 'password'
 screenshot_dir = join(DIR, 'screenshot')
+
 
 @pytest.fixture(scope="module")
 def module_setup(request):
@@ -26,34 +24,32 @@ def module_teardown(driver, mobile_driver):
     driver.close()
     mobile_driver.close()
 
-def test_internal_ui(driver, app_domain, device_host):
 
+def test_internal_ui(driver, device_host):
     driver.get("http://{0}:81".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(2)
     screenshots(driver, screenshot_dir, 'activate')
     print(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []'))
 
 
-def teat_lpgin(driver, mobile_driver, app_domain, device_host):
-    _test_login(driver, 'desktop', app_domain, device_host)
-    _test_login(mobile_driver, 'mobile', app_domain, device_host)
+def teat_lpgin(driver, mobile_driver, device_host):
+    _test_login(driver, 'desktop', device_host)
+    _test_login(mobile_driver, 'mobile', device_host)
 
 
-def _test_login(driver, mode, app_domain, device_host):
-
+def _test_login(driver, mode, device_host):
     driver.get("http://{0}".format(device_host))
     time.sleep(2)
     screenshots(driver, screenshot_dir, 'login-' + mode)
     print(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []'))
 
 
-def test_index(driver, mobile_driver, device_host):
-    _test_index(driver, 'desktop', device_host)
-    _test_index(mobile_driver, 'mobile', device_host)
+def test_index(driver, mobile_driver):
+    _test_index(driver, 'desktop')
+    _test_index(mobile_driver, 'mobile')
 
 
-def _test_index(driver, mode, device_host):
+def _test_index(driver, mode):
     user = driver.find_element_by_id("name")
     user.send_keys(DEVICE_USER)
     password = driver.find_element_by_id("password")
@@ -64,74 +60,76 @@ def _test_index(driver, mode, device_host):
     time.sleep(5)
     screenshots(driver, screenshot_dir, 'index-' + mode)
 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
 
 
 def test_settings(driver, device_host):
-    
     driver.get("http://{0}/settings.html".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(5)
     screenshots(driver, screenshot_dir, 'settings')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
 
 
 def test_settings_activation(driver, device_host):
-
     driver.get("http://{0}/activation.html".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'settings_activation')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
 
 
 def test_settings_network(driver, device_host):
-
     driver.get("http://{0}/network.html".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'settings_network')
-    
+
     driver.find_element_by_css_selector(".bootstrap-switch-id-tgl_external").click()
     time.sleep(2)
     screenshots(driver, screenshot_dir, 'settings_network_external_access')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
 
 def test_settings_storage(driver, device_host):
     url = "http://{0}/storage.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'settings_storage')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
 
 def test_settings_updates(driver, device_host):
     url = "http://{0}/updates.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'settings_updates')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
 
 def test_settings_support(driver, device_host):
     url = "http://{0}/support.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'settings_support')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
 
 def test_app_center(driver, mobile_driver, device_host):
     _test_app_center(driver, 'desktop', device_host)
@@ -143,28 +141,28 @@ def _test_app_center(driver, mode, device_host):
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'appcenter-' + mode)
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
-    assert not driver.execute_script('return document.documentElement.scrollWidth>document.documentElement.clientWidth;')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+    assert not driver.execute_script(
+        'return document.documentElement.scrollWidth>document.documentElement.clientWidth;')
+
 
 def test_installed_app(driver, device_host):
-
     driver.get("http://{0}/app.html?app_id=files".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'app_installed')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
 
 
 def test_not_installed_app(driver, device_host):
-
     driver.get("http://{0}/app.html?app_id=nextcloud".format(device_host))
-    wait_driver = WebDriverWait(driver, 10)
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'app_not_installed')
- 
-    assert not driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
+
+    assert not driver.execute_script(
+        'return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')
