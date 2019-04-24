949b985048007e4c3d3a9b8bdd6b05abf6a50a92
Boris Rybalkin <ribalkin@gmail.com>
2019-04-24 21:45:05 +0100

    remove ldapi and permissions for now

--- src/syncloud_platform/auth/ldapauth.py
+++ src/syncloud_platform/auth/ldapauth.py
@@ -14,6 +14,7 @@ import time
 ldap_user_conf_dir = 'slapd.d'
 DOMAIN="dc=syncloud,dc=org"
 
+
 class LdapAuth:
     def __init__(self, platform_config, systemctl):
         self.systemctl = systemctl
