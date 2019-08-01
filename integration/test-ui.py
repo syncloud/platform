cdb59881afe50b103f5eb52deafe223756d7a3c3
Boris Rybalkin <ribalkin@gmail.com>
2019-08-01 23:35:53 +0100

    fix screenshots

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -56,53 +56,53 @@ def test_index(driver, ui_mode, device_user, device_password):
     screenshots(driver, screenshot_dir, 'index-' + ui_mode)
 
 
-def test_settings(driver, device_host):
+def test_settings(driver, device_host, ui_mode):
     driver.get("http://{0}/settings.html".format(device_host))
     time.sleep(5)
-    screenshots(driver, screenshot_dir, 'settings')
+    screenshots(driver, screenshot_dir, 'settings-' + ui_mode)
 
 
-def test_settings_activation(driver, device_host):
+def test_settings_activation(driver, device_host, ui_mode):
     driver.get("http://{0}/activation.html".format(device_host))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'settings_activation')
+    screenshots(driver, screenshot_dir, 'settings_activation-' + ui_mode)
 
 
-def test_settings_network(driver, device_host):
+def test_settings_network(driver, device_host, ui_mode):
     driver.get("http://{0}/network.html".format(device_host))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'settings_network')
+    screenshots(driver, screenshot_dir, 'settings_network-' + ui_mode)
 
     driver.find_element_by_css_selector(".bootstrap-switch-id-tgl_external").click()
     time.sleep(2)
-    screenshots(driver, screenshot_dir, 'settings_network_external_access')
+    screenshots(driver, screenshot_dir, 'settings_network_external_access-' + ui_mode)
 
 
-def test_settings_storage(driver, device_host):
+def test_settings_storage(driver, device_host, ui_mode):
     url = "http://{0}/storage.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'settings_storage')
+    screenshots(driver, screenshot_dir, 'settings_storage-' + ui_mode)
 
 
-def test_settings_updates(driver, device_host):
+def test_settings_updates(driver, device_host, ui_mode):
     url = "http://{0}/updates.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'settings_updates')
+    screenshots(driver, screenshot_dir, 'settings_updates-' + ui_mode)
 
 
-def test_settings_support(driver, device_host):
+def test_settings_support(driver, device_host, ui_mode):
     url = "http://{0}/support.html".format(device_host)
     resp = requests.get(url, verify=False)
     assert resp.status_code == 200
     driver.get(url)
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'settings_support')
+    screenshots(driver, screenshot_dir, 'settings_support-' + ui_mode)
 
 
 def test_settings_backup(driver, ui_mode, device_host):
@@ -123,13 +123,13 @@ def test_app_center(driver, ui_mode, device_host):
     screenshots(driver, screenshot_dir, 'appcenter-' + ui_mode)
 
 
-def test_installed_app(driver, device_host):
+def test_installed_app(driver, device_host, ui_mode):
     driver.get("http://{0}/app.html?app_id=files".format(device_host))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'app_installed')
+    screenshots(driver, screenshot_dir, 'app_installed-' + ui_mode)
 
 
-def test_not_installed_app(driver, device_host):
+def test_not_installed_app(driver, device_host, ui_mode):
     driver.get("http://{0}/app.html?app_id=nextcloud".format(device_host))
     time.sleep(10)
-    screenshots(driver, screenshot_dir, 'app_not_installed')
+    screenshots(driver, screenshot_dir, 'app_not_installed-' + ui_mode)
