90737a9be2976c08ed01fec27946ca566a752597
Boris Rybalkin <ribalkin@gmail.com>
2019-08-28 16:31:19 +0300

    fix import

--- src/syncloud_platform/gaplib/linux.py
+++ src/syncloud_platform/gaplib/linux.py
@@ -1,4 +1,4 @@
-from subprocess import check_output, call
+from subprocess import check_output, call, CalledProcessError
 import pwd
 from os import environ
 import massedit
@@ -42,7 +42,7 @@ def local_ip():
 def local_ip_v6():
     try:
         return check_output("/snap/platform/current/bin/cli ipv6", shell=True)
-    except CalledProcessError e:
+    except CalledProcessError, e:
         return None
 
 def is_ip_public(ip):
