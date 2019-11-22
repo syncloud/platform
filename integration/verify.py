7854a4fb6a8c691aa7eff9891d52270e8877b14d
Boris Rybalkin <ribalkin@gmail.com>
2019-11-22 18:59:13 +0000

    fix verify

--- integration/verify.py
+++ integration/verify.py
@@ -357,12 +357,12 @@ def test_do_not_cache_static_files_as_we_get_stale_ui_on_upgrades(device, device
 
 def test_installer_upgrade(device, app_domain):
     session = device.login()
-    response = device.http_get('/rest/installer/upgrade', verify=False)
+    response = device.http_get('/rest/installer/upgrade')
     assert response.status_code == 200, response.text
     wait_for_response(session, 'https://{0}/rest/job/status'.format(app_domain),
                       lambda r:  json.loads(r.text)['data'] == 'JobStatusIdle')
 
-    response = device.http_get('/rest/installer/upgrade', verify=False)
+    response = device.http_get('/rest/installer/upgrade')
     assert response.status_code == 200, response.text
     wait_for_response(session, 'https://{0}/rest/job/status'.format(app_domain),
                       lambda r:  json.loads(r.text)['data'] == 'JobStatusIdle')
