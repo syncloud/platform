71d16f69aad2a5c4f2cc8506ee88b8b514560761
Boris Rybalkin <ribalkin@gmail.com>
2019-11-22 06:43:27 +0000

    fix channel

--- backend/installer/installer.go
+++ backend/installer/installer.go
@@ -18,7 +18,7 @@ func New() *Installer {
 
 func (installer *Installer) Upgrade() {
 	log.Println("Running installer upgrade", INSTALLER_UPGRADE_CMD)
-	out, err := exec.Command(INSTALLER_UPGRADE_CMD).CombinedOutput()
+	out, err := exec.Command(INSTALLER_UPGRADE_CMD, "stable").CombinedOutput()
 	log.Printf("Installer upgrade output %s", out)
 	if err != nil {
 		log.Printf("Installer upgrade failed: %v", err)
