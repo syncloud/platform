9c0e5b38e3b4bc0cca7d3c9b693a6890a342284a
Boris Rybalkin <ribalkin@gmail.com>
2019-11-22 08:55:16 +0000

    fix installer upgrade wait

--- integration/verify.py
+++ integration/verify.py
@@ -358,10 +358,13 @@ def test_do_not_cache_static_files_as_we_get_stale_ui_on_upgrades(device, device
 def test_installer_upgrade(device, device_host):
     session = device.login()
     session.get('https://{0}/rest/installer/upgrade'.format(device_host), verify=False)
-    wait_for_installer(session, device_host, throw_on_error=True)
-    session.get('https://{0}/rest/installer/upgrade'.format(device_host), verify=False)
-    wait_for_installer(session, device_host, throw_on_error=True)
+    wait_for_response(session, 'https://{0}/rest/job/status'.format(app_domain),
+                      lambda r:  json.loads(r.text)['data'] == 'JobStatusIdle')
 
+    session.get('https://{0}/rest/installer/upgrade'.format(device_host), verify=False)
+    wait_for_response(session, 'https://{0}/rest/job/status'.format(app_domain),
+                      lambda r:  json.loads(r.text)['data'] == 'JobStatusIdle')
+   
 
 def test_backup_app(device, artifact_dir, app_domain):
     
