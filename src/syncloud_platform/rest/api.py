1acc2d38254753f6c7b1f9a635b2eb35864fb93b
Boris Rybalkin <ribalkin@gmail.com>
2019-12-30 11:51:44 +0300

    fix config methods

--- src/syncloud_platform/rest/api.py
+++ src/syncloud_platform/rest/api.py
@@ -52,14 +52,14 @@ def init_storage():
     return jsonify(success=True, message='', data=app_storage_dir), 200
 
 
-@app.route("/config/dkim_key", methods=["POST"])
+@app.route("/config/set_dkim_key", methods=["POST"])
 def config_dkim_key():
     dkim_key = request.form['dkim_key']
     get_injector().user_platform_config.set_dkim_key(dkim_key)
     return jsonify(success=True, message='dkim_key set'), 200
 
 
-@app.route("/config/dkim_key", methods=["GET"])
+@app.route("/config/get_dkim_key", methods=["GET"])
 def config_dkim_key():
     dkim_key = get_injector().user_platform_config.get_dkim_key()
     return jsonify(success=True, message='dkim_key', data=dkim_key), 200
