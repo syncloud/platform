03778a0d8856d09b68736d22ff8142a11a9e362b
Boris Rybalkin <ribalkin@gmail.com>
2020-01-20 16:17:15 +0000

    ipv4 public

--- backend/cmd/cli/main.go
+++ backend/cmd/cli/main.go
@@ -11,8 +11,9 @@ import (
 func main() {
 
 	var cmdIpv4 = &cobra.Command{
-		Use:   "ipv4",
+		Use:   "ipv4 [public]",
 		Short: "Print IPv4",
+		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			ip, err := network.LocalIPv4()
 			if err != nil {
@@ -21,6 +22,20 @@ func main() {
 			fmt.Print(ip.String())
 		},
 	}
+	var cmdIpv4public = &cobra.Command{
+		Use:   "public",
+		Short: "Print public IPv4",
+		Args:  cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			ip, err := network.PublicIPv4()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print(ip)
+		},
+	}
+	cmdIpv4.AddCommand(cmdIpv4public)
+
 	var cmdIpv6 = &cobra.Command{
 		Use:   "ipv6 [prefix]",
 		Short: "Print IPv6",
@@ -35,7 +50,7 @@ func main() {
 	}
 	var prefixSize int
 	var cmdIpv6prefix = &cobra.Command{
-		Use:   "prefix size",
+		Use:   "prefix",
 		Short: "Print IPv6 prefix",
 		//Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
--- backend/network/iface.go
+++ backend/network/iface.go
@@ -1,7 +1,9 @@
 package network
 
 import (
+	"io/ioutil"
 	"net"
+	"net/http"
 )
 
 func LocalIPv4() (net.IP, error) {
@@ -22,4 +24,21 @@ func LocalIPv6() (net.IP, error) {
 	defer conn.Close()
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP, nil
+}
+
+func PublicIPv4() (string, error) {
+	url := "https://api.ipify.org?format=text"
+	// http://myexternalip.com
+	// http://api.ident.me
+	// http://whatismyipaddress.com/api
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(ip), nil
 }
