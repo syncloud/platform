7b9ffc3eb122175b4fde4284dd0994c9223a07b0
Boris Rybalkin <ribalkin@gmail.com>
2020-03-24 18:04:51 +0000

    snap artifact

--- build.sh
+++ build.sh
@@ -103,3 +103,5 @@ PACKAGE=${NAME}_${VERSION}_${ARCH}.snap
 echo ${PACKAGE} > package.name
 
 mksquashfs ${SNAP_DIR} ${DIR}/${PACKAGE} -noappend -comp xz -no-xattrs -all-root
+mkdir ${DIR}/artifact
+cp ${DIR}/${PACKAGE} ${DIR}/artifact
