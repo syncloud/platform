454e6155a8e136d9d57873ee564a93acc2326dc9
Boris Rybalkin <ribalkin@gmail.com>
2020-04-10 19:20:32 +0100

    fix hide_fields_errors reference

--- www/js/activate.js
+++ www/js/activate.js
@@ -14,27 +14,27 @@ import * as Common from './common.js'
 
 
 function activate(parameters, on_always, on_done, on_error) {
-        $.post("/rest/activate", parameters)
-            .done(on_done)
-            .fail(on_error)
-            .always(on_always);
+    $.post("/rest/activate", parameters)
+        .done(on_done)
+        .fail(on_error)
+        .always(on_always);
 }
 
 function activate_custom_domain(parameters, on_always, on_done, on_error) {
     $.post("/rest/activate_custom_domain", parameters)
-            .done(on_done)
-            .fail(on_error)
-            .always(on_always);
+        .done(on_done)
+        .fail(on_error)
+        .always(on_always);
 }
 
 function login() {
-        var url = (new URI())
-                .protocol('https')
-                .port(443)
-                .filename("")
-                .query("");
+    var url = (new URI())
+            .protocol('https')
+            .port(443)
+            .filename("")
+            .query("");
 
-        window.location.href = url;
+    window.location.href = url;
 }
 
 
@@ -42,48 +42,48 @@ $(document).ready(function () {
 
     if (typeof mock !== 'undefined') { console.log("backend mock") };
 
-        $("#domain_type_syncloud").click(function (event) {
-				event.preventDefault();
-				$("#domain_type").val('syncloud');
-		});
+    $("#domain_type_syncloud").click(function (event) {
+            event.preventDefault();
+            $("#domain_type").val('syncloud');
+    });
 
-		$("#domain_type_custom").click(function (event) {
-				event.preventDefault();
-				$("#domain_type").val('custom');
-		});
+    $("#domain_type_custom").click(function (event) {
+            event.preventDefault();
+            $("#domain_type").val('custom');
+    });
 
     $("#btn_error_send_logs").on('click', function () {
         window.location.href = "sendlogs.html";
     });
 
-		$("#form_activate").submit(function (event) {
-				event.preventDefault();
+    $("#form_activate").submit(function (event) {
+        event.preventDefault();
 
-				var values = $("#form_activate").serializeArray();
+        var values = $("#form_activate").serializeArray();
 
-				var btn = $('#btn_activate');
-				btn.button('loading');
-				$("#form_activate input").prop("disabled", true);
-				hide_fields_errors("form_activate");
+        var btn = $('#btn_activate');
+        btn.button('loading');
+        $("#form_activate input").prop("disabled", true);
+        UiCommon.hide_fields_errors("form_activate");
 
-				var on_always = function() {
-					btn.button('reset');
-					$("#form_activate input").prop("disabled", false);
-				};
+        var on_always = function() {
+            btn.button('reset');
+            $("#form_activate input").prop("disabled", false);
+        };
 
-   				if ( $("#domain_type").val() == 'syncloud') {
-					activate(
-						values,
-						on_always,
-						backend.login,
-						ui_display_error);
-				} else {
-					activate_custom_domain(
-						values,
-						on_always,
-						backend.login,
-						ui_display_error);
-				}
-		});
+        if ( $("#domain_type").val() == 'syncloud') {
+            activate(
+                values,
+                on_always,
+                backend.login,
+                ui_display_error);
+        } else {
+            activate_custom_domain(
+                values,
+                on_always,
+                backend.login,
+                ui_display_error);
+        }
+    });
 });
 
