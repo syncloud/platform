4fa2952bf1b523cfcc532dcb45cc4d5ed28e68b9
Sebastian <44033489+klmhsb42@users.noreply.github.com>
2020-05-11 19:18:11 +0200

    Reorganize and add app info
    Reorganize and add app info

--- www/js/app.templates.js
+++ www/js/app.templates.js
@@ -4,8 +4,27 @@ const AppTemplate = `
 				<div>
 					<img src="<%= info.app.icon %>" class="appimg" alt="">
 				</div>
+
 				<div class="appinfo">
 				<h1><%= info.app.name %></h1>
+
+
+
+
+<div id="app_info" class="modal fade bs-are-use-sure" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">App information</h4>
+			</div>
+			<div class="modal-body">
+				<div class="bodymod">
+					<div class="btext">
+					<h2><%= info.app.name %></h2>
+					<div class="btext"><%= info.app.description %></div>
+			<h2>Details</h2>
+
         	<% if (info.installed_version !== null) { %>
 					<b>Installed version:</b> <%= info.installed_version %><br>
         	<% } %>
@@ -13,6 +32,26 @@ const AppTemplate = `
 					<b>Available version:</b> <%= info.current_version %><br>
         	<% } %>
 					<!--<b>Size:</b> 17.0 MB-->
+					<!--<b>Website:</b> <a href="" target="_blank">content</a>-->
+					<!--<b>Licence:</b> content-->
+					<!--<b>Last update:</b> content-->
+					<!--<b>Channel:</b> content-->
+					
+					</div>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn buttonlight bwidth smbutton" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
 				</div>
 			</div>
 			<div>
@@ -32,8 +71,10 @@ const AppTemplate = `
         <% if (info.installed_version !== null) { %>
 					<button id="btn_backup"  class="buttonblue bwidth smbutton" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'></i> Creating backup...">Backup</button>
         	<% } %>
+        	<button data-toggle="modal" data-target="#app_info" type="button" class="control" style="background:transparent;">
+				<i class='fa fa-info-circle fa-lg'></i>
+			</button>
 				</div>
-				<div class="btext"><%= info.app.description %></div>
 			</div>
 		</div>
 `
