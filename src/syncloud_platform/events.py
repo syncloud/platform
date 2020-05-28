73347366c863e6651b1c7b3e83c0b7d5dc1a3d9a
Boris Rybalkin <ribalkin@gmail.com>
2020-05-28 21:26:35 +0100

    print event stderr if available

--- src/syncloud_platform/events.py
+++ src/syncloud_platform/events.py
@@ -19,12 +19,14 @@ class EventTrigger:
             app_id = app.app.id
             try:
                 info = check_output('snap info {0}'.format(app_id), shell=True).decode()
-                command = '{0}.{1}'.format(app_id, action)
+                command = 'snap run {0}.{1}'.format(app_id, action)
                 if command in info:
-                    self.log.info('executing {0}: {1}'.format(app_id, action))
-                    output = check_output('snap run {0}'.format(command), shell=True).decode()
+                    self.log.info('executing {0}'.format(command))
+                    output = check_output(command, shell=True).decode()
                     print(output)
             except CalledProcessError as e:
-                self.log.error('event error: {0}'.format(e.output.decode()))
+                self.log.error('event output {0}'.format(e.output.decode()))
+                if e.stderr:
+                    self.log.error('event error {0}'.format(e.stderr.decode()))
                 self.log.error(traceback.format_exc())
 
