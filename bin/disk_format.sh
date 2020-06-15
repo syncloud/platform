71ade9989f56c7aab74039796617ee0af95cdc4f
Boris Rybalkin <ribalkin@gmail.com>
2020-06-15 18:16:09 +0100

    switch to gpt for external disks (#522)

--- bin/disk_format.sh
+++ bin/disk_format.sh
@@ -1,27 +1,18 @@
 #!/bin/bash -e
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+
 if [[ -z "$1" ]]; then
     echo "usage $0 device"
     exit 1
 fi
 
 DEVICE=$1
-
 PARTITION=1
 
 dd if=/dev/zero of=${DEVICE} bs=512 count=1 conv=notrunc
-
-echo "
-o
-p
-n
-p
-${PARTITION}
-
-
-p
-w
-q
-" | fdisk ${DEVICE}
-
-mkfs.ext4 ${DEVICE}${PARTITION}
+export LD_LIBRARY_PATH=${DIR}/gptfdisk/lib
+${DIR}/gptfdisk/bin/sgdisk -o ${DEVICE}
+${DIR}/gptfdisk/bin/sgdisk -n ${PARTITION} ${DEVICE}
+${DIR}/gptfdisk/bin/sgdisk -p ${DEVICE}
+mkfs.ext4 -F ${DEVICE}${PARTITION}
