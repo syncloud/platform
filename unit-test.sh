8c0eb0fb395110c5506c0ce93de8f80f287a2193
Boris Rybalkin <ribalkin@gmail.com>
2021-03-20 15:38:51 +0000

    print dependencies

--- unit-test.sh
+++ unit-test.sh
@@ -4,6 +4,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 NAME="platform"
 BUILD_DIR=${DIR}/build/${NAME}
+
+${BUILD_DIR}/python/bin/pip install pipdetree
+${BUILD_DIR}/python/bin/pipdetree
+
 ${BUILD_DIR}/python/bin/pip install -r ${DIR}/dev_requirements.txt
 
 cd ${DIR}/src
