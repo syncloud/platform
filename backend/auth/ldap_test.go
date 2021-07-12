ae46eb98c6ffe059c41f0b3cd1c5cc6e1833fad6
Boris Rybalkin <ribalkin@gmail.com>
2021-07-12 21:53:42 +0100

    remove unused code

--- backend/auth/ldap_test.go
+++ backend/auth/ldap_test.go
@@ -5,12 +5,6 @@ import (
 	"testing"
 )
 
-//func TestGenerateChangePasswordCmd(t *testing.T) {
-//	assert.Equal(t, GenerateChangePasswordCmd("123123"), "echo \"root:123123\" | chpasswd")
-//	assert.Equal(t, GenerateChangePasswordCmd("123\"123"), "echo \"root:123\\\"123\" | chpasswd")
-//	assert.Equal(t, GenerateChangePasswordCmd("123$123"), "echo \"root:123\\$123\" | chpasswd")
-//}
-
 func TestToLdapDc(t *testing.T) {
 	assert.Equal(t, ToLdapDc("user.syncloud.it"), "dc=user,dc=syncloud,dc=it")
 }
--- src/test/test_injector.py
+++
@@ -1,5 +0,0 @@
-from syncloud_platform.injector import get_injector
-
-def test_injecter():
-    #injector = get_injector()
-    assert True
