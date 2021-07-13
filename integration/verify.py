a93dfa835859a4937a9a3d0e503f492be421c77b
Boris Rybalkin <ribalkin@gmail.com>
2021-07-13 23:49:53 +0100

    fix test

--- integration/verify.py
+++ integration/verify.py
@@ -227,7 +227,7 @@ def test_activation_status_true(device_host):
 
 
 def test_redirect_info(device_host, main_domain):
-    response = requests.get('https://{0}/redirect/info'.format(device_host), allow_redirects=False,
+    response = requests.get('https://{0}/rest/redirect/info'.format(device_host), allow_redirects=False,
                             verify=False)
     assert response.status_code == 200
     assert json.loads(response.text)["domain"] == main_domain, response.text
