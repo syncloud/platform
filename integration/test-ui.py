15a0e6927c9d1368251518030d8cbafdc0a2b082
Boris Rybalkin <ribalkin@gmail.com>
2021-08-06 23:58:16 +0300

    fix ui tear down

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -28,7 +28,8 @@ def module_setup(request, device, artifact_dir, ui_mode, data_dir):
     request.addfinalizer(module_teardown)
 
 
-#def test_start(app, device_host, module_setup):
+def test_start(app, device_host, module_setup):
+    pass
 #    add_host_alias(app, device_host)
 
 
