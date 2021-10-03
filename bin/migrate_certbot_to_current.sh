0d1989af49e8d247864d1f34bf1f39f2bd96d606
Boris Rybalkin <ribalkin@gmail.com>
2021-10-03 14:56:14 +0100

    fix migrate conf check

--- bin/migrate_certbot_to_current.sh
+++ bin/migrate_certbot_to_current.sh
@@ -4,6 +4,6 @@ OLD_CERTBOT=/var/snap/platform/common/certbot
 NEW_CERTBOT=/var/snap/platform/current/certbot
 
 RENEWAL_DIR=$NEW_CERTBOT/renewal
-if [[ -d $RENEWAL_DIR ]]; then
+if ls -la $RENEWAL_DIR/*.conf; then
     sed -i 's#'$OLD_CERTBOT'#'$NEW_CERTBOT'#g' $RENEWAL_DIR/*.conf
 fi
