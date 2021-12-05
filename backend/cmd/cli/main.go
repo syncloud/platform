d0e032201c649d059d409a0c54ac79be666925cb
Boris Rybalkin <ribalkin@gmail.com>
2021-12-05 10:22:10 +0000

    fix cli

--- backend/cmd/cli/main.go
+++ backend/cmd/cli/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/golobby/container/v3"
 	"github.com/spf13/cobra"
 	"github.com/syncloud/platform/config"
 	"github.com/syncloud/platform/cron"
@@ -17,16 +16,22 @@ func main() {
 	log.SetFlags(0)
 	log.SetOutput(&logger.Logger{})
 
+	var rootCmd = &cobra.Command{Use: "cli"}
+	configDb := rootCmd.PersistentFlags().String("config", config.DefaultConfigDb, "sqlite config db")
+
 	var cmdIpv4 = &cobra.Command{
 		Use:   "ipv4 [public]",
 		Short: "Print IPv4",
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			ip, err := network.New().LocalIPv4()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Print(ip.String())
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(iface *network.Interface) {
+				ip, err := iface.LocalIPv4()
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Print(ip.String())
+			})
 		},
 	}
 	var cmdIpv4public = &cobra.Command{
@@ -34,11 +39,14 @@ func main() {
 		Short: "Print public IPv4",
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			ip, err := network.New().PublicIPv4()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Print(ip)
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(iface *network.Interface) {
+				ip, err := iface.PublicIPv4()
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Print(ip)
+			})
 		},
 	}
 	cmdIpv4.AddCommand(cmdIpv4public)
@@ -48,11 +56,14 @@ func main() {
 		Short: "Print IPv6",
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			ip, err := network.New().IPv6()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Print(ip.String())
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(iface *network.Interface) {
+				ip, err := iface.IPv6()
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Print(ip.String())
+			})
 		},
 	}
 	var prefixSize int
@@ -60,11 +71,14 @@ func main() {
 		Use:   "prefix",
 		Short: "Print IPv6 prefix",
 		Run: func(cmd *cobra.Command, args []string) {
-			ip, err := network.New().IPv6()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("%v/%v", ip.Mask(net.CIDRMask(prefixSize, 128)), prefixSize)
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(iface *network.Interface) {
+				ip, err := iface.IPv6()
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Printf("%v/%v", ip.Mask(net.CIDRMask(prefixSize, 128)), prefixSize)
+			})
 		},
 	}
 	cmdIpv6prefix.Flags().IntVarP(&prefixSize, "size", "s", 64, "Prefix size")
@@ -82,15 +96,13 @@ func main() {
 		Short: "Set config key value",
 		Args:  cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
-			var configuration *config.UserConfig
-			err := container.Resolve(configuration)
-			if err != nil {
-				log.Fatal(err)
-			}
-			key := args[0]
-			value := args[1]
-			configuration.Upsert(key, value)
-			log.Printf("set config: %s, key: %s, value: %s\n", configFile, key, value)
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(configuration *config.UserConfig) {
+				key := args[0]
+				value := args[1]
+				configuration.Upsert(key, value)
+				log.Printf("set config: %s, key: %s, value: %s\n", configFile, key, value)
+			})
 		},
 	}
 	cmdConfig.AddCommand(cmdConfigSet)
@@ -100,12 +112,10 @@ func main() {
 		Short: "Get config key value",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			var configuration *config.UserConfig
-			err := container.Resolve(configuration)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println(configuration.Get(args[0], ""))
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(configuration *config.UserConfig) {
+				log.Println(configuration.Get(args[0], ""))
+			})
 		},
 	}
 	cmdConfig.AddCommand(cmdConfigGet)
@@ -115,14 +125,12 @@ func main() {
 		Short: "List config key value",
 		Args:  cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			var configuration *config.UserConfig
-			err := container.Resolve(configuration)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for key, value := range configuration.List() {
-				log.Printf("%s:%s\n", key, value)
-			}
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(configuration *config.UserConfig) {
+				for key, value := range configuration.List() {
+					log.Printf("%s:%s\n", key, value)
+				}
+			})
 		},
 	}
 	cmdConfig.AddCommand(cmdConfigList)
@@ -131,19 +139,12 @@ func main() {
 		Use:   "cron",
 		Short: "Run cron job",
 		Run: func(cmd *cobra.Command, args []string) {
-			var cronService *cron.Cron
-			err := container.Resolve(cronService)
-			if err != nil {
-				log.Fatal(err)
-			}
-			cronService.StartSingle()
+			ioc.Init(*configDb, config.File)
+			ioc.Call(func(cronService *cron.Cron) { cronService.StartSingle() })
 		},
 	}
 
-	var rootCmd = &cobra.Command{Use: "cli"}
 	rootCmd.AddCommand(cmdIpv4, cmdIpv6, cmdConfig, cmdCron)
-
-	ioc.Init(config.DefaultConfigDb, config.File)
 
 	err := rootCmd.Execute()
 	if err != nil {
