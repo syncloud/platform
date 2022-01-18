a36dd57a29325c74b223abee5d06c7bec8f2a59f
Boris Rybalkin <ribalkin@gmail.com>
2022-01-18 19:12:15 +0000

    more store test logs

--- integration/test-store.py
+++ integration/test-store.py
@@ -1,9 +1,22 @@
 import os
 
+import pytest
+from subprocess import check_output
 from syncloudlib.integration.hosts import add_host_alias
 
+TMP_DIR = '/tmp/syncloud'
 
-def test_start(app, device_host, domain):
+
+@pytest.fixture(scope="session")
+def module_setup(request, device, artifact_dir):
+    def module_teardown():
+        device.run_ssh('journalctl > {0}/store-test.journalctl.log'.format(TMP_DIR), throw=False)
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+
+    request.addfinalizer(module_teardown)
+
+
+def test_start(module_setup, app, device_host, domain):
     add_host_alias(app, device_host, domain)
 
 
