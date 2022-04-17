fa6f08449d53fb98f3c14639bfec483228931bbd
Boris Rybalkin <ribalkin@gmail.com>
2022-04-17 16:18:47 +0100

    fix test

--- src/test/insider/test_device_info.py
+++ src/test/insider/test_device_info.py
@@ -11,7 +11,7 @@ def test_url_activated_free():
     user_platform_config.set_manual_access_port(10000)
     user_platform_config.set_redirect_enabled(True)
 
-    device_info = DeviceInfo(user_platform_config, port_config)
+    device_info = DeviceInfo(user_platform_config)
 
     assert device_info.url('app') == 'https://app.device.syncloud.it:10000'
 
@@ -24,7 +24,7 @@ def test_url_activated_custom():
     user_platform_config.set_manual_access_port(10000)
     user_platform_config.set_redirect_enabled(False)
 
-    device_info = DeviceInfo(user_platform_config, port_config)
+    device_info = DeviceInfo(user_platform_config)
 
     assert device_info.url('app') == 'https://app.example.com:10000'
 
@@ -37,6 +37,6 @@ def test_url_non_activated():
     user_platform_config.set_manual_access_port(10000)
     user_platform_config.set_redirect_enabled(False)
 
-    device_info = DeviceInfo(user_platform_config, port_config)
+    device_info = DeviceInfo(user_platform_config)
 
     assert device_info.url('app') == 'https://app.localhost:10000'
