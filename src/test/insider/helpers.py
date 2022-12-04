ef0b9302aff310593bf7691d1c6ce2cba7587cf6
Boris Rybalkin <ribalkin@gmail.com>
2022-12-04 16:37:34 +0000

    more py cleanup

--- src/test/insider/helpers.py
+++ src/test/insider/helpers.py
@@ -1,7 +1,5 @@
-from os.path import dirname, join
 import tempfile
 import os
-from syncloud_platform.config.config import PLATFORM_CONFIG_NAME, PlatformConfig
 from syncloud_platform.config.user_config import PlatformUserConfig
 
 
@@ -18,21 +16,7 @@ def temp_file(text='', filename=None):
     return filename
 
 
-test_conf_dir = join(dirname(__file__), 'conf')
-test_services_config_file_name = 'services.json'
-test_services_config_file = join(test_conf_dir, test_services_config_file_name)
-
-CONFIG_DIR = join(dirname(__file__), '..', '..', '..', 'config')
-
-platform_config_file = CONFIG_DIR
-
-
 def get_user_platform_config():
     config = PlatformUserConfig(temp_file())
     config.init_user_config()
-    return config
-
-
-def get_platform_config():
-    config = PlatformConfig(platform_config_file)
     return config
