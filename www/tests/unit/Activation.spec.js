684e719b2dcafda4244f8139c52b0d8e24f4793e
Boris Rybalkin <ribalkin@gmail.com>
2022-12-18 08:22:06 +0000

    fix activation tests

--- www/tests/unit/Activation.spec.js
+++ www/tests/unit/Activation.spec.js
@@ -7,27 +7,13 @@ import { ElButton } from 'element-plus'
 
 jest.setTimeout(30000)
 
-test('Activate free domain', async () => {
-  let redirectEmail = ''
-  let redirectPassword = ''
-  let domain = ''
-  let deviceUsername = ''
-  let devicePassword = ''
-  let availabilityDomain = ''
+test('Activation url', async () => {
+  
   const showError = jest.fn()
   const mockRouter = { push: jest.fn() }
   delete window.location
   window.location = ''
   const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/managed').reply(function (config) {
-    const request = JSON.parse(config.data)
-    redirectEmail = request.redirect_email
-    redirectPassword = request.redirect_password
-    domain = request.domain
-    deviceUsername = request.device_username
-    devicePassword = request.device_password
-    return [200, { success: true }]
-  })
   mock.onPost('/rest/redirect/domain/availability').reply(function (config) {
     const request = JSON.parse(config.data)
     availabilityDomain = request.domain
@@ -35,9 +21,9 @@ test('Activate free domain', async () => {
     redirectPassword = request.password
     return [200, { success: true }]
   })
-  mock.onGet('/rest/redirect_info').reply(200, { success: true, data: { domain: 'test.com' } })
+  mock.onGet('/rest/device/url').reply(200, { success: true, data: 'test.com' })
 
-  const wrapper = mount(Activate,
+  const wrapper = mount(Activation,
     {
       attachTo: document.body,
       global: {
@@ -57,168 +43,30 @@ test('Activate free domain', async () => {
       }
     }
   )
-
-  await flushPromises()
-
-  await wrapper.find('#btn_free_domain').trigger('click')
-  await wrapper.find('#email').setValue('r email')
-  await wrapper.find('#redirect_password').setValue('r password')
-  await wrapper.find('#domain_input').setValue('domain')
-  await wrapper.find('#btn_next').trigger('click')
-  await wrapper.find('#device_username').setValue('user')
-  await wrapper.find('#device_password').setValue('password')
-  await wrapper.find('#btn_activate').trigger('click')
 
   await flushPromises()
 
   expect(showError).toHaveBeenCalledTimes(0)
-  expect(availabilityDomain).toBe('domain.test.com')
-  expect(redirectEmail).toBe('r email')
-  expect(redirectPassword).toBe('r password')
-  expect(domain).toBe('domain.test.com')
-  expect(deviceUsername).toBe('user')
-  expect(devicePassword).toBe('password')
-  expect(window.location).toMatch(new RegExp('^/\\?t=.*'))
-
+  expect(wrapper.find('#txt_device_domain').text()).toBe('test.com')
+  
   wrapper.unmount()
 })
 
-test('Activate free domain error', async () => {
-  let error = ''
-  const showError = (err) => {
-    error = err.response.data.message
-  }
-  const mockRouter = { push: jest.fn() }
-
-  const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/managed').reply(500, {
-    message: 'not ok'
-  })
-
-  mock.onPost('/rest/redirect/domain/availability').reply(200, {
-    message: 'ok'
-  })
-  mock.onGet('/rest/redirect_info').reply(200, { success: true, data: { domain: 'test.com' } })
-
-  const wrapper = mount(Activate,
-    {
-      attachTo: document.body,
-      global: {
-        stubs: {
-          Error: {
-            template: '<span/>',
-            methods: {
-              showAxios: showError
-            }
-          },
-          Dialog: true,
-          'el-button': ElButton,
-        },
-        mocks: {
-          $router: mockRouter
-        }
-      }
-    }
-  )
-
-  await flushPromises()
-  await wrapper.find('#btn_free_domain').trigger('click')
-  await wrapper.find('#email').setValue('r email')
-  await wrapper.find('#redirect_password').setValue('r password')
-  await wrapper.find('#domain_input').setValue('domain')
-  await wrapper.find('#btn_next').trigger('click')
-  await wrapper.find('#device_username').setValue('user')
-  await wrapper.find('#device_password').setValue('password')
-  await wrapper.find('#btn_activate').trigger('click')
-
-  await flushPromises()
-
-  expect(error).toBe('not ok')
-
-  wrapper.unmount()
-})
-
-test('Activate free domain availability error', async () => {
-  let error = ''
-  const showError = (err) => {
-    error = err.response.data.parameters_messages[0].messages[0]
-  }
-  const mockRouter = { push: jest.fn() }
-
-  const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/managed').reply(500, {
-    message: 'not ok'
-  })
-
-  mock.onPost('/rest/redirect/domain/availability').reply(400, {
-    success: false,
-    parameters_messages: [
-      { parameter: 'domain', messages: ['domain is already taken'] }
-    ]
-  })
-  mock.onGet('/rest/redirect_info').reply(200, { success: true, data: { domain: 'test.com' } })
-
-  const wrapper = mount(Activate,
-    {
-      attachTo: document.body,
-      global: {
-        stubs: {
-          Error: {
-            template: '<span/>',
-            methods: {
-              showAxios: showError
-            }
-          },
-          Dialog: true,
-          'el-button': ElButton,
-        },
-        mocks: {
-          $router: mockRouter
-        }
-      }
-    }
-  )
-
-  await flushPromises()
-  await wrapper.find('#btn_free_domain').trigger('click')
-  await wrapper.find('#email').setValue('r email')
-  await wrapper.find('#redirect_password').setValue('r password')
-  await wrapper.find('#domain_input').setValue('domain')
-  await wrapper.find('#btn_next').trigger('click')
-
-  await flushPromises()
-
-  expect(error).toBe('domain is already taken')
-
-  wrapper.unmount()
-})
-
-test('Activate custom domain', async () => {
-  let domain = ''
-  let deviceUsername = ''
-  let devicePassword = ''
+test('Activation reactivate', async () => {
+  
   const showError = jest.fn()
   const mockRouter = { push: jest.fn() }
   delete window.location
   window.location = ''
-
+  let deactivated = false
   const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/custom').reply(function (config) {
-    const request = JSON.parse(config.data)
-    domain = request.domain
-    deviceUsername = request.device_username
-    devicePassword = request.device_password
+  mock.onPost('/rest/settings/deactivate').reply(function (config) {
+    deactivated = true
     return [200, { success: true }]
   })
+  mock.onGet('/rest/device/url').reply(200, { success: true, data: 'test.com' })
 
-  let availabilityCalled = false
-  mock.onPost('/rest/redirect/domain/availability').reply(function (_) {
-    availabilityCalled = true
-    return [200, { success: true }]
-  })
-  mock.onGet('/rest/redirect_info').reply(200, { success: true, data: { domain: 'test.com' } })
-
-  const wrapper = mount(Activate,
+  const wrapper = mount(Activation,
     {
       attachTo: document.body,
       global: {
@@ -241,174 +89,14 @@ test('Activate custom domain', async () => {
 
   await flushPromises()
 
-  await wrapper.find('#btn_custom_domain').trigger('click')
-  await wrapper.find('#domain').setValue('domain')
-  await wrapper.find('#device_username').setValue('user')
-  await wrapper.find('#device_password').setValue('password')
-  await wrapper.find('#btn_next').trigger('click')
-
-  await wrapper.find('#btn_activate').trigger('click')
+  expect(showError).toHaveBeenCalledTimes(0)
+  expect(wrapper.find('#txt_device_domain').text()).toBe('test.com')
+  wrapper.find('#btn_reactivate').trigger('click')
 
   await flushPromises()
 
-  expect(showError).toHaveBeenCalledTimes(0)
-  expect(domain).toBe('domain')
-  expect(deviceUsername).toBe('user')
-  expect(devicePassword).toBe('password')
-  expect(window.location).toMatch(new RegExp('^/\\?t=.*'))
-  expect(availabilityCalled).toBe(false)
+  expect(deactivated).toBe(true)
 
   wrapper.unmount()
 })
 
-test('Activate premium domain', async () => {
-  let redirectEmail = ''
-  let redirectPassword = ''
-  let domain = ''
-  let deviceUsername = ''
-  let devicePassword = ''
-  let availabilityDomain = ''
-  const showError = jest.fn()
-  const mockRouter = { push: jest.fn() }
-  delete window.location
-  window.location = ''
-  const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/managed').reply(function (config) {
-    const request = JSON.parse(config.data)
-    redirectEmail = request.redirect_email
-    redirectPassword = request.redirect_password
-    domain = request.domain
-    deviceUsername = request.device_username
-    devicePassword = request.device_password
-    return [200, { success: true }]
-  })
-  mock.onPost('/rest/redirect/domain/availability').reply(function (config) {
-    const request = JSON.parse(config.data)
-    availabilityDomain = request.domain
-    redirectEmail = request.email
-    redirectPassword = request.password
-    return [200, { success: true }]
-  })
-  mock.onGet('/rest/redirect_info').reply(200, { success: true, data: { domain: 'test.com' } })
-
-  const wrapper = mount(Activate,
-    {
-      attachTo: document.body,
-      global: {
-        stubs: {
-          Error: {
-            template: '<span/>',
-            methods: {
-              showAxios: showError
-            }
-          },
-          Dialog: true,
-          'el-button': ElButton,
-        },
-        mocks: {
-          $router: mockRouter
-        }
-      }
-    }
-  )
-
-  await flushPromises()
-
-  await wrapper.find('#btn_premium_domain').trigger('click')
-  await wrapper.find('#email').setValue('r email')
-  await wrapper.find('#redirect_password').setValue('r password')
-  await wrapper.find('#domain_premium').setValue('example.com')
-  await wrapper.find('#btn_next').trigger('click')
-  await wrapper.find('#device_username').setValue('user')
-  await wrapper.find('#device_password').setValue('password')
-  await wrapper.find('#btn_activate').trigger('click')
-
-  await flushPromises()
-
-  expect(showError).toHaveBeenCalledTimes(0)
-  expect(redirectEmail).toBe('r email')
-  expect(redirectPassword).toBe('r password')
-  expect(domain).toBe('example.com')
-  expect(deviceUsername).toBe('user')
-  expect(devicePassword).toBe('password')
-  expect(window.location).toMatch(new RegExp('^/\\?t=.*'))
-  expect(availabilityDomain).toBe('example.com')
-
-  wrapper.unmount()
-})
-
-test('Activated while page is open (mostly in local dev)', async () => {
-  let redirectEmail = ''
-  let redirectPassword = ''
-  let domain = ''
-  let deviceUsername = ''
-  let devicePassword = ''
-  let availabilityDomain = ''
-  const showError = jest.fn()
-  const mockRouter = { push: jest.fn() }
-  delete window.location
-  window.location = ''
-  const mock = new MockAdapter(axios)
-  mock.onPost('/rest/activate/managed').reply(function (config) {
-    const request = JSON.parse(config.data)
-    redirectEmail = request.redirect_email
-    redirectPassword = request.redirect_password
-    domain = request.domain
-    deviceUsername = request.device_username
-    devicePassword = request.device_password
-    return [200, { success: true }]
-  })
-  mock.onPost('/rest/redirect/domain/availability').reply(function (config) {
-    const request = JSON.parse(config.data)
-    availabilityDomain = request.domain
-    redirectEmail = request.email
-    redirectPassword = request.password
-    return [200, { success: true }]
-  })
-  mock.onGet('/rest/redirect_info').reply(502, { message: 'Device is already activated' })
-
-  const wrapper = mount(Activate,
-    {
-      attachTo: document.body,
-      global: {
-        stubs: {
-          Error: {
-            template: '<span/>',
-            methods: {
-              showAxios: showError
-            }
-          },
-          Dialog: true,
-          'el-button': ElButton,
-        },
-        mocks: {
-          $router: mockRouter
-        }
-      }
-    }
-  )
-
-  await flushPromises()
-
-  await wrapper.find('#btn_premium_domain').trigger('click')
-  await wrapper.find('#email').setValue('r email')
-  await wrapper.find('#redirect_password').setValue('r password')
-  await wrapper.find('#domain_premium').setValue('example.com')
-  await wrapper.find('#btn_next').trigger('click')
-  await wrapper.find('#device_username').setValue('user')
-  await wrapper.find('#device_password').setValue('password')
-  await wrapper.find('#btn_activate').trigger('click')
-
-  await flushPromises()
-
-  expect(showError).toHaveBeenCalledTimes(0)
-  expect(redirectEmail).toBe('r email')
-  expect(redirectPassword).toBe('r password')
-  expect(domain).toBe('example.com')
-  expect(deviceUsername).toBe('user')
-  expect(devicePassword).toBe('password')
-  expect(window.location).toMatch(new RegExp('^/\\?t=.*'))
-  expect(availabilityDomain).toBe('example.com')
-
-  wrapper.unmount()
-})
