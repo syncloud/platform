812c8888237ec3f14f9cbd56ce36618967cbc959
Boris Rybalkin <ribalkin@gmail.com>
2023-03-16 17:27:02 +0000

    drop post start

--- bin/service.nginx-public.sh
+++ bin/service.nginx-public.sh
@@ -12,9 +12,6 @@ start)
     ${DIR}/nginx/sbin/nginx -t -c /var/snap/platform/current/nginx.conf -e stderr
     exec $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -e stderr
     ;;
-post-start)
-    timeout 5 /bin/bash -c 'until echo > /dev/tcp/localhost/80; do sleep 1; done'
-    ;;
 reload)
     $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -s reload -e stderr
     ;;
