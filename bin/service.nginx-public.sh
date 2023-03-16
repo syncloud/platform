a406db92764b7e0f089cebf605d948c072b62361
Boris Rybalkin <ribalkin@gmail.com>
2023-03-16 08:50:53 +0000

    restore reload

--- bin/service.nginx-public.sh
+++ bin/service.nginx-public.sh
@@ -2,7 +2,28 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-${DIR}/nginx/sbin/nginx -t -c /var/snap/platform/current/nginx.conf -e stderr
-exec $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -e stderr
+if [[ -z "$1" ]]; then
+    echo "usage $0 [start|stop]"
+    exit 1
+fi
 
+case $1 in
+start)
+    ${DIR}/nginx/sbin/nginx -t -c /var/snap/platform/current/nginx.conf -e stderr
+    exec $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -e stderr
+    ;;
+post-start)
+    timeout 5 /bin/bash -c 'until echo > /dev/tcp/localhost/80; do sleep 1; done'
+    ;;
+reload)
+    $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -s reload -e stderr
+    ;;
+stop)
+    $DIR/nginx/sbin/nginx -c /var/snap/platform/current/nginx.conf -s stop -e stderr
+    ;;
+*)
+    echo "not valid command"
+    exit 1
+    ;;
+esac
 
