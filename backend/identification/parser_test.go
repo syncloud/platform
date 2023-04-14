e3eb1d4e6eca8e1fbe1029c9f42a043473553c29
Boris Rybalkin <ribalkin@gmail.com>
2023-04-14 22:28:11 +0100

    some unit tests

--- backend/identification/parser_test.go
+++ backend/identification/parser_test.go
@@ -2,6 +2,8 @@ package identification
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
 	"testing"
 )
 
@@ -9,4 +11,35 @@ func TestMac(t *testing.T) {
 	mac, err := GetMac()
 	assert.Nil(t, err)
 	assert.NotEmpty(t, mac)
+}
+
+func TestParser_Id(t *testing.T) {
+	tempDir := t.TempDir()
+	idFile := path.Join(tempDir, "id")
+	err := os.WriteFile(idFile, []byte(`
+[id]
+name=name
+title=title
+`), 0644)
+	assert.NoError(t, err)
+	parser := &Parser{filename: idFile}
+	id, err := parser.Id()
+	assert.NoError(t, err)
+	assert.Equal(t, "name", id.Name)
+	assert.Equal(t, "title", id.Title)
+}
+
+func TestParser_Id_NoIdSection(t *testing.T) {
+	tempDir := t.TempDir()
+	idFile := path.Join(tempDir, "id")
+	err := os.WriteFile(idFile, []byte(`
+name=name
+title=title
+`), 0644)
+	assert.NoError(t, err)
+	parser := &Parser{filename: idFile}
+	id, err := parser.Id()
+	assert.NoError(t, err)
+	assert.Equal(t, "unknown", id.Name)
+	assert.Equal(t, "Unknown", id.Title)
 }
