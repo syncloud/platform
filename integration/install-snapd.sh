fd7e1a2975ea6ef56ef38a3548a7045b4bfd13c0
Boris Rybalkin <ribalkin@gmail.com>
2023-07-18 20:48:44 +0100

    migrate to snapd v2 with store

--- integration/install-snapd.sh
+++ integration/install-snapd.sh
@@ -1,39 +1,13 @@
 #!/bin/bash -xe
 
+VERSION=$(curl http://apps.syncloud.org/releases/stable/snapd2.version)
 ARCH=$(dpkg --print-architecture)
-VERSION=$(curl http://apps.syncloud.org/releases/stable/snapd.version)
-
 SNAPD=snapd-${VERSION}-${ARCH}.tar.gz
-wget http://apps.syncloud.org/apps/${SNAPD} --progress=dot:giga
 
-tar xzvf ${SNAPD}
-systemctl stop snapd.service snapd.socket || true
-systemctl disable snapd.service snapd.socket || true
-
-rm -rf /var/lib/snapd
-mkdir /var/lib/snapd
-
-rm -rf /usr/lib/snapd
-mkdir -p /usr/lib/snapd
-cp snapd/bin/snapd /usr/lib/snapd
-cp snapd/bin/snap-exec /usr/lib/snapd
-cp snapd/bin/snap-confine /usr/lib/snapd
-cp snapd/bin/snap-seccomp /usr/lib/snapd
-cp snapd/bin/snap-repair /usr/lib/snapd
-cp snapd/bin/snap-update-ns /usr/lib/snapd
-cp snapd/bin/snap-discard-ns /usr/lib/snapd
-cp snapd/bin/snap /usr/bin
-cp snapd/bin/snapctl /usr/bin
-cp snapd/bin/mksquashfs /usr/bin
-cp snapd/bin/unsquashfs /usr/bin
-cp snapd/lib/* /lib/$HOSTTYPE-$OSTYPE
-
-cp snapd/conf/snapd.service /lib/systemd/system/
-cp snapd/conf/snapd.socket /lib/systemd/system/
-
-systemctl enable snapd.service
-systemctl enable snapd.socket
-systemctl start snapd.service snapd.socket
-
-snap --version
-
+cd /tmp
+rm -rf "${SNAPD}"
+rm -rf snapd
+wget http://apps.syncloud.org/apps/"${SNAPD}" --progress=dot:giga
+tar xzvf "${SNAPD}"
+mkdir -p /var/lib/snapd/snaps
+./snapd/install.sh
