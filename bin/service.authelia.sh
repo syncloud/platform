0de734fd455d4cf0261a3ee4984045c1420992b8
Boris Rybalkin <ribalkin@gmail.com>
2024-05-12 20:42:49 +0100

    authelia: disable unused expand-env plugin

--- bin/service.authelia.sh
+++ bin/service.authelia.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 exec ${DIR}/auth/authelia.sh \
   --config /var/snap/platform/current/config/authelia/config.yml \
-  --config.experimental.filters expand-env,template
+  --config.experimental.filters template
