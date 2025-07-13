0d03cae0f621ace560f681c329201eb70973be05
Boris Rybalkin <ribalkin@gmail.com>
2025-07-13 16:04:22 +0100

    less deps

--- test/deps.sh
+++ test/deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 apt-get update
-apt-get install -y sshpass openssh-client netcat rustc file libxml2-dev libxslt-dev build-essential libz-dev curl apache2-utils
+apt-get install -y sshpass openssh-client netcat-openbsd file curl apache2-utils
 pip install -r requirements.txt
