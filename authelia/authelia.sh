d73097f778df9e7e3d4c7d0efdddab1a9d391047
Boris Rybalkin <ribalkin@gmail.com>
2026-02-28 22:52:06 +0000

    Fix authelia wrapper library path to include arch subdirectory
    The bundled libs are in lib/arm-linux-gnueabihf/ (or equivalent),
    not directly in lib/. Use the *-linux-gnu* glob for library path,
    matching the pattern used by gogs/postgresql wrappers.

--- authelia/authelia.sh
+++ authelia/authelia.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LD=$(find ${DIR}/lib -name 'ld-*.so*' -type f -print -quit)
-exec ${LD} --library-path ${DIR}/lib ${DIR}/authelia "$@"
+LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
+exec ${LD} --library-path $LIBS ${DIR}/authelia "$@"
