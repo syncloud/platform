773368f78fb6f86b219ec8c6cadebaeabc7ab404
Boris Rybalkin <ribalkin@gmail.com>
2026-02-28 22:32:34 +0000

    Fix authelia packaging: dereference /lib symlink when copying
    The Authelia container has /lib -> usr/lib symlink. cp -r copies the
    broken symlink. Use cp -rL to follow symlinks and copy actual files.

--- authelia/package.sh
+++ authelia/package.sh
@@ -5,5 +5,5 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap/authelia
 mkdir -p ${BUILD_DIR}
 cp /app/authelia ${BUILD_DIR}
-cp -r /lib ${BUILD_DIR}
+cp -rL /lib ${BUILD_DIR}
 cp ${DIR}/authelia.sh ${BUILD_DIR}
