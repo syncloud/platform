2df10ccf408df787d30ddc763e49b0f662adca74
Boris Rybalkin <ribalkin@gmail.com>
2026-02-28 22:13:10 +0000

    Fix authelia wrapper to use glibc ld-linux pattern from other projects
    Use lib/*-linux*/ld-linux*.so* pattern matching gogs/postgresql and
    other glibc-based projects in the codebase.

--- authelia/authelia.sh
+++ authelia/authelia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export LD_LIBRARY_PATH=${DIR}/lib
-${DIR}/authelia "$@"
+LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
+exec ${DIR}/lib/*-linux*/ld-linux*.so* --library-path $LIBS ${DIR}/authelia "$@"
