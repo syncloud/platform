8ee1276cfc1ec94b94cadccdad854c025eb58a24
Boris Rybalkin <ribalkin@gmail.com>
2026-03-17 13:46:32 +0000

    Extract defocus() helper for screenshot stabilization

--- test/ui.py
+++ test/ui.py
@@ -72,13 +72,11 @@ def test_activate(selenium, device_host,
     selenium.find_by_id('btn_free_domain').click()
     wait_for(selenium, lambda: selenium.find_by_id('email').send_keys(""))
     selenium.find_by_id('email').send_keys(redirect_user)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-redirect-email')
     selenium.find_by_id('redirect_password').send_keys(redirect_password)
     selenium.find_by_id('domain_input').send_keys(domain)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-type')
     selenium.find_by_id('btn_next').click()
     wait_for_loading(selenium.driver)
@@ -89,14 +87,12 @@ def test_activate(selenium, device_host,
     selenium.find_by_id('device_username').send_keys(device_user)
     selenium.find_by_id('device_password').send_keys(device_password)
     selenium.find_by_id('device_password_confirm').send_keys(device_password)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-ready')
     selenium.find_by_id('btn_activate').click()
     wait_for_loading(selenium.driver)
     selenium.find_by(By.ID, "username-textfield")
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate')
 
 
@@ -105,8 +101,7 @@ def test_login(selenium, full_domain, device_user, device_password):
     # OIDC flow redirects to Authelia
     selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
     selenium.find_by(By.ID, "password-textfield").send_keys(device_password)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('login')
     selenium.find_by(By.ID, "sign-in-button").click()
     selenium.find_by_xpath("//h1[text()='Applications']")
@@ -304,8 +299,7 @@ def test_auth_web(selenium, full_domain, device_user, device_password):
     selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
     password = selenium.find_by(By.ID, "password-textfield")
     password.send_keys(device_password)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('auth')
     selenium.find_by(By.ID, "sign-in-button").click()
 
@@ -408,13 +402,11 @@ def test_settings_deactivate(selenium, device_host, full_domain,
     selenium.find_by_id('btn_free_domain').click()
     wait_for(selenium, lambda: selenium.find_by_id('email').send_keys(""))
     selenium.find_by_id('email').send_keys(redirect_user)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-redirect-email')
     selenium.find_by_id('redirect_password').send_keys(redirect_password)
     selenium.find_by_id('domain_input').send_keys(domain)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-type')
     selenium.find_by_id('btn_next').click()
     wait_for_loading(selenium.driver)
@@ -425,14 +417,12 @@ def test_settings_deactivate(selenium, device_host, full_domain,
     selenium.find_by_id('device_username').send_keys(device_user)
     selenium.find_by_id('device_password').send_keys(device_password)
     selenium.find_by_id('device_password_confirm').send_keys(device_password)
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('activate-ready')
     selenium.find_by_id('btn_activate').click()
     wait_for_loading(selenium.driver)
     selenium.find_by(By.ID, "username-textfield")
-    selenium.driver.find_element(By.TAG_NAME, "body").click()
-    time.sleep(1)
+    defocus(selenium)
     selenium.screenshot('deactivate-login-page')
     selenium.find_by(By.ID, "username-textfield").send_keys(device_user)
     selenium.find_by(By.ID, "password-textfield").send_keys(device_password)
@@ -501,6 +491,11 @@ def wait_for(selenium, method):
         retry += 1
     selenium.screenshot('exception')
     raise exception
+
+
+def defocus(selenium):
+    selenium.driver.find_element(By.TAG_NAME, "body").click()
+    time.sleep(1)
 
 
 def settings(selenium, setting):
